bc0bdaa962e79ae994ef892dda56240ef4d110a6
Quan Brew <t@ioover.net>
2019-08-23 11:26:34 +0800

    fix dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,7 @@
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './settings';
-
-require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

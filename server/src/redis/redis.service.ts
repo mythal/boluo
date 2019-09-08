c3114197905957ba5791f700217799f52de65a73
Quan Brew <t@ioover.net>
2019-09-08 15:17:21 +0800

    fix dependencies and redis

--- server/src/redis/redis.service.ts
+++ server/src/redis/redis.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { RedisClient } from 'redis';
 import { REDIS_DB, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } from '../settings';
+import * as IORedis from 'ioredis';
 
 const Redis = require('ioredis');
 
 @Injectable()
 export class RedisService {
-  public client: RedisClient;
+  public client: IORedis.Redis;
 
   constructor() {
     this.client = new Redis({
@@ -19,21 +19,13 @@ export class RedisService {
 
   set(key: string, value: string, expiration: number = 0) {
     if (expiration > 0) {
-      this.client.setex(key, expiration, value);
+      this.client.setex(key, expiration, value).then();
     } else {
-      this.client.set(key, value);
+      this.client.set(key, value).then();
     }
   }
 
   get(key: string): Promise<string | null> {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+    return this.client.get(key);
   }
 }

64f88da33f0ee2fcf0585fc7bd11dfe1d83bec2a
Quan Brew <t@ioover.net>
2019-09-12 05:34:43 +0800

    rate limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './settings';
 
+const rateLimit = require('express-rate-limit');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    })
+  );
 }
 
 bootstrap().catch(console.error);

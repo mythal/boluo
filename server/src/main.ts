7fe845d63144c62cf31510ca0d002e70a03d679c
Quan Brew <t@ioover.net>
2019-09-13 04:24:59 +0800

    enable CORS on debug mode

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,23 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './settings';
+import { DEBUG, PORT } from './settings';
 
 const rateLimit = require('express-rate-limit');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    })
-  );
+  if (DEBUG) {
+    app.enableCors();
+  } else {
+    app.use(
+      rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 150, // limit each IP to 100 requests per windowMs
+      })
+    );
+  }
 }
 
 bootstrap().catch(console.error);

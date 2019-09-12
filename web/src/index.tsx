14caf1df8283f554d8ac1bdd0127aba265eb597c
Quan Brew <t@ioover.net>
2019-09-13 03:15:49 +0800

    remove demo, restart the front-end

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { ApolloProvider } from '@apollo/react-hooks';
-import * as serviceWorker from './serviceWorker';
 import { client } from './apollo';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById('app-root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

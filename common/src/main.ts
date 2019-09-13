27115692d5757b604232afb18e7b8305c6f5b956
Quan Brew <t@ioover.net>
2019-09-13 23:41:15 +0800

    common explicit export

--- common/src/main.ts
+++ common/src/main.ts
@@ -1,3 +1,26 @@
-export * from './entities';
-export * from './parser';
-export * from './validators';
+export {
+  Text,
+  Link,
+  Entity,
+  ExprNode,
+  Expr,
+  Emphasis,
+  Operator,
+  Mention,
+  BaseEntity,
+  Strong,
+  Roll,
+  Binary,
+  Num,
+} from './entities';
+export { parse, Env, ParseResult } from './parser';
+export {
+  checkNickname,
+  checkMessage,
+  checkChannelTitle,
+  checkChannelName,
+  checkPassword,
+  checkUsername,
+  checkEmailFormat,
+  checkCharacterName,
+} from './validators';

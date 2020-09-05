f61b9564816274278ee0ba92e179c46b26b25b03
coppa <sortal@protonmail.com>
2020-09-06 04:04:00 +0800

    merge key down

--- src/components/chat/key.ts
+++ src/components/chat/key.ts
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import { ComposeDispatch, update } from './compose/reducer';
 
+let submitKeyHandle: number | undefined = undefined;
+
+const submitKey = (callback: () => void) => {
+  window.clearTimeout(submitKeyHandle);
+  submitKeyHandle = window.setTimeout(callback, 200);
+};
+
 export const handleKeyDown = (
   composeDispatch: ComposeDispatch,
   onSend: () => void,
   inGame: boolean
 ): React.KeyboardEventHandler => {
-  return async (e) => {
+  return (e) => {
     if ((e.metaKey || e.ctrlKey) && e.key === 'Enter') {
       e.preventDefault();
-      await onSend();
+      submitKey(async () => await onSend());
     } else if (e.key === 'Alt') {
       e.preventDefault();
-      composeDispatch(update({ inGame: !inGame }));
+      submitKey(() => composeDispatch(update({ inGame: !inGame })));
     }
   };
 };

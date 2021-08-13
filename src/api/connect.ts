4a5cf127a6660e54f149164a874cef2d5a2053e3
Tachibana Kiyomi <me@yuru.me>
2021-08-14 06:12:10 +0900

    fix build

--- src/api/connect.ts
+++ src/api/connect.ts
@@ -1,6 +1,7 @@
 import { MailboxType } from './events';
 import { Id } from '../utils/id';
-import { DEBUG } from 'settings';
+
+const DEBUG = process.env.NODE_ENV === 'development';
 
 export const connect = (id: Id, type: MailboxType, after: number): WebSocket => {
   const { host, protocol } = window.location;

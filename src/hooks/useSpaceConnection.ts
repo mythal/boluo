e8c3226b2dbfe271d2abed346dca825cdd9e00a2
Tachibana Kiyomi <me@yuru.me>
2021-08-27 05:09:48 +0900

    reconnection

--- src/hooks/useSpaceConnection.ts
+++ src/hooks/useSpaceConnection.ts
@@ -3,12 +3,14 @@ import { useEffect, useRef } from 'react';
 import { connect } from '../api/connect';
 import { Events } from '../api/events';
 import { connectSpace, SpaceUpdated } from '../actions/ui';
+import { showFlash } from '../actions/flash';
 
 export function useSpaceConnection() {
   const dispatch = useDispatch();
   const spaceId = useSelector((state) => state.ui.spaceId);
 
   const after = useRef<number>(0);
+  const retry = useRef<number>(0);
 
   const conn = async (): Promise<WebSocket> => {
     if (!spaceId) {
@@ -18,9 +20,14 @@ export function useSpaceConnection() {
     connection.onerror = (e) => {
       console.warn(e);
     };
+    connection.onopen = () => {
+      if (retry.current > 1) {
+        dispatch(showFlash('SUCCESS', '已经重新连接上了菠萝'));
+      }
+      retry.current = 0;
+    };
 
     connection.onmessage = (wsMsg) => {
-      const last = after.current;
       const event = JSON.parse(wsMsg.data) as Events;
       // if (event.timestamp < last) {
       //   return;
@@ -35,10 +42,16 @@ export function useSpaceConnection() {
         dispatch({ type: 'EVENT_RECEIVED', event });
       }
     };
-    // connection.onclose = (e) => {
-    //   console.warn(e);
-    //   window.setTimeout(() => {dispatch}, 5000);
-    // };
+    connection.onclose = (e) => {
+      window.setTimeout(() => {
+        console.log('reconnecting');
+        if (retry.current > 1) {
+          dispatch(showFlash('WARNING', '连接已断开，正在尝试重连。'));
+        }
+        retry.current += 1;
+        conn().catch(console.error);
+      }, Math.floor(Math.random() * 5000));
+    };
     return connection;
   };
 

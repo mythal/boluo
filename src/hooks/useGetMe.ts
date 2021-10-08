b31a3b9b813c76d3b9bb63192ba36eb63d62918a
Tachibana Kiyomi <me@yuru.me>
2021-10-09 07:35:32 +0900

    don't log out when failed failed to access API

--- src/hooks/useGetMe.ts
+++ src/hooks/useGetMe.ts
@@ -6,11 +6,13 @@ export const useGetMe = (dispatch: Dispatch, finish: () => void): void => {
   useEffect(() => {
     const loadMe = async () => {
       const me = await get('/users/get_me');
-      if (me.isOk && me.value !== null) {
-        const { user, mySpaces, myChannels, settings } = me.value;
-        dispatch({ type: 'LOGGED_IN', user, myChannels, mySpaces, settings });
-      } else {
-        dispatch({ type: 'LOGGED_OUT' });
+      if (me.isOk) {
+        if (me.value) {
+          const { user, mySpaces, myChannels, settings } = me.value;
+          dispatch({ type: 'LOGGED_IN', user, myChannels, mySpaces, settings });
+        } else {
+          dispatch({ type: 'LOGGED_OUT' });
+        }
       }
     };
     loadMe().then(() => finish());

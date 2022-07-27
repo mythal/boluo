8d5ad52ee9b1ef04dbde89b31c0ebfdc995e821a
Tachibana Kiyomi <me@yuru.me>
2022-07-28 05:39:14 +0900

    some configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 // https://webpack.js.org/guides/
 
+require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');

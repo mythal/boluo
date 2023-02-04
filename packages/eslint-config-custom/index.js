44e7110a673a34af4a2914c002acb89d8d8bf4d0
Tachibana Kiyomi <me@yuru.me>
2023-02-04 16:40:55 +0900

    chore: add project related files

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,19 @@
+// Fix `Parsing error: Cannot find module 'next/babel'`
+//
+// https://github.com/vercel/next.js/issues/40687#issuecomment-1264177674
+
+/**
+* @type {import('eslint').Linter.Config}
+*/
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
   },
 };

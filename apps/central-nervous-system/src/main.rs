c595101a2bd8d6f72e3c8098e7b697eea1aec3f0
ギャラ <me@gyara.moe>
2023-03-11 15:28:14 +0900

    chore: Create framework
    chore: Comp nav and main
    chore: Reset style

--- apps/central-nervous-system/src/main.rs
+++ apps/central-nervous-system/src/main.rs
@@ -1,3 +1,19 @@
+use yew::{function_component, html, Html};
+
+mod nav;
+
+#[function_component(App)]
+fn app() -> Html {
+    html!(
+        <>
+            <nav::Nav />
+            <main>
+                {"hello world"}
+            </main>
+        </>
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }

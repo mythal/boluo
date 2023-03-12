89f6b06bae2218cfdd4afdb721d1f104eda906ff
ギャラ <me@gyara.moe>
2023-03-12 18:00:24 +0900

    feat(apps/central-nervous-system): Add route

--- apps/central-nervous-system/src/main.rs
+++ apps/central-nervous-system/src/main.rs
@@ -1,6 +1,21 @@
 use yew::{function_component, html, Html};
+use yew_router::Routable;
 
 mod nav;
+
+#[derive(Clone, Copy, Routable, PartialEq, Debug)]
+pub enum Route {
+    #[at("/")]
+    Root,
+    #[at("/users")]
+    Users,
+    #[at("/user/:id")]
+    User { id: String },
+    #[at("/tasks")]
+    Tasks,
+    #[at("/task/:id")]
+    Task { id: String },
+}
 
 #[function_component(App)]
 fn app() -> Html {

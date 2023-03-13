0031272acb6a14811b11ec45a4d20b776318b4b0
Tachibana Kiyomi <me@yuru.me>
2023-03-13 15:03:00 +0900

    feat(chat): show content in pane header without positioned

--- apps/chat/src/components/PaneHeaderBox.tsx
+++ apps/chat/src/components/PaneHeaderBox.tsx
@@ -9,25 +9,32 @@ interface Props {
   children: ReactNode;
   operators?: ReactNode;
   icon?: ReactNode;
+  extra?: ReactNode;
 }
 
-export const PaneHeaderBox: FC<Props> = ({ children, operators, icon }) => {
+export const PaneHeaderBox: FC<Props> = ({ children, operators, icon, extra }) => {
   const isFocused = useIsFocused();
   const paneBanner = usePaneBanner();
   return (
-    <div
-      className={clsx(
-        'relative min-h-pane-header bg-surface-100 flex items-center px-4 text-lg',
-        'justify-between border-b-1/2 transition-colors duration-200',
-        isFocused ? 'border-brand-600' : 'border-surface-300',
-      )}
-    >
-      <div className="inline-flex gap-1 items-center flex-nowrap whitespace-nowrap overflow-hidden">
-        {icon}
-        {children}
+    <div className={paneBanner.content || extra ? 'border-b border-surface-300' : ''}>
+      <div
+        className={clsx(
+          'min-h-pane-header bg-surface-100 flex items-center px-4 text-lg',
+          'justify-between border-b-1/2 transition-colors duration-200',
+          isFocused ? 'border-brand-600' : 'border-surface-300',
+        )}
+      >
+        <div className="inline-flex gap-1 items-center flex-nowrap whitespace-nowrap overflow-hidden">
+          {icon}
+          {children}
+        </div>
+        {operators && <div className="inline-flex gap-1 ml-2" onClick={stopPropagation}>{operators}</div>}
       </div>
-      {operators && <div className="inline-flex gap-1 ml-2" onClick={stopPropagation}>{operators}</div>}
-      {paneBanner.content && <PaneBanner banner={paneBanner} />}
+
+      <div className="divide-y">
+        {paneBanner.content && <PaneBanner banner={paneBanner} />}
+        {extra}
+      </div>
     </div>
   );
 };

1bf9a8184de6912826189e0a1f6e1406ef73c509
Tachibana Kiyomi <me@yuru.me>
2023-04-10 16:21:07 +0900

    feat(chat): name display

--- apps/chat/src/components/channel/ChatItemSwitch.tsx
+++ apps/chat/src/components/channel/ChatItemSwitch.tsx
@@ -5,15 +5,24 @@ import { ChatItemPreview } from './ChatItemPreview';
 
 interface Props {
   chatItem: ChatItem;
+  myId: string | undefined;
   className?: string;
+  isContinuous: boolean;
 }
 
-export const ChatItemSwitch: FC<Props> = ({ chatItem, className = '' }) => {
+export const ChatItemSwitch: FC<Props> = ({ chatItem, className = '', myId, isContinuous }) => {
   switch (chatItem.type) {
     case 'MESSAGE':
-      return <ChatItemMessage message={chatItem} className={className} />;
+      return (
+        <ChatItemMessage
+          self={myId === chatItem.senderId}
+          message={chatItem}
+          className={className}
+          isContinuous={isContinuous}
+        />
+      );
     case 'PREVIEW':
-      return <ChatItemPreview preview={chatItem} className={className} />;
+      return <ChatItemPreview self={myId === chatItem.senderId} preview={chatItem} className={className} />;
     default:
       return <div className={className}>Not implemented</div>;
   }

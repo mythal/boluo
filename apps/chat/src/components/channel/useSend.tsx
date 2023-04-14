966261a7ffba861c2ba43637d6b18ba0e83e9315
Tachibana Kiyomi <me@yuru.me>
2023-04-14 18:22:05 +0900

    feat(chat): better messages sending

--- apps/chat/src/components/channel/useSend.tsx
+++ apps/chat/src/components/channel/useSend.tsx
@@ -1,9 +1,67 @@
-import { createContext, useContext } from 'react';
+import { User } from 'api';
+import { usePost } from 'common';
+import { useStore } from 'jotai';
+import { useCallback } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Button } from 'ui';
+import { useSetBanner } from '../../hooks/useBanner';
+import { useChannelId } from '../../hooks/useChannelId';
+import { useComposeAtom } from '../../hooks/useComposeAtom';
+import { ComposeActionUnion, makeComposeAction } from '../../state/actions/compose';
 
-export const SendContext = createContext<() => void>(() => {
-  console.error('Invaild `send`');
-});
+export const useSend = (me: User) => {
+  const channelId = useChannelId();
+  const store = useStore();
+  const composeAtom = useComposeAtom();
+  const setBanner = useSetBanner();
+  const post = usePost();
+  const { nickname } = me;
 
-export const useSend = (): () => void => {
-  return useContext(SendContext);
+  const send = useCallback(async () => {
+    const compose = store.get(composeAtom);
+    if (compose.error !== null) {
+      return;
+    }
+    const backupComposeState = compose;
+    const dispatch = (action: ComposeActionUnion) => store.set(composeAtom, action);
+    const handleRecover = () => {
+      dispatch(makeComposeAction('recoverState', backupComposeState));
+      setBanner(null);
+    };
+    dispatch(makeComposeAction('setSource', { channelId, source: '', range: [0, 0] }));
+
+    const result = await post('/messages/send', null, {
+      messageId: null,
+      previewId: compose.previewId,
+      channelId,
+      name: compose.inputedName.trim() || nickname,
+      text: compose.source,
+      entities: [],
+      inGame: compose.inGame,
+      isAction: false,
+      mediaId: null,
+      pos: null,
+      whisperToUsers: null,
+    });
+    if (result.isOk) {
+      return;
+    }
+    setBanner({
+      level: 'WARNING',
+      content: (
+        <div className="">
+          <FormattedMessage defaultMessage="Error while sending a message, did you recover the message?" />
+          <Button
+            data-small
+            className="ml-2"
+            onClick={handleRecover}
+          >
+            <FormattedMessage defaultMessage="Recover" />
+          </Button>
+        </div>
+      ),
+    });
+  }, [channelId, composeAtom, nickname, post, setBanner, store]);
+
+  return send;
 };
--- apps/chat/src/components/compose/SendButton.tsx
+++ apps/chat/src/components/compose/SendButton.tsx
@@ -1,83 +1,25 @@
 import type { GetMe } from 'api';
-import { usePost } from 'common';
 import { PaperPlane } from 'icons';
-import { useAtom } from 'jotai';
-import { forwardRef, useCallback, useImperativeHandle, useMemo } from 'react';
+import { useAtomValue } from 'jotai';
+import { selectAtom } from 'jotai/utils';
+import { FC, forwardRef, useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Button } from 'ui/Button';
-import { useSetBanner } from '../../hooks/useBanner';
-import { useChannelId } from '../../hooks/useChannelId';
 import { useComposeAtom } from '../../hooks/useComposeAtom';
-import { makeComposeAction } from '../../state/actions/compose';
-import { useSendPreview } from './useSendPreview';
+import { useSend } from '../channel/useSend';
 
 interface Props {
   me: GetMe;
 }
 
-export interface SendRef {
-  send: () => void;
-}
-
-export const SendButton = forwardRef<SendRef, Props>(({ me }, ref) => {
-  const channelId = useChannelId();
+export const SendButton: FC<Props> = ({ me }) => {
   const composeAtom = useComposeAtom();
-  const [compose, dispatch] = useAtom(composeAtom);
-  const setBanner = useSetBanner();
-  const post = usePost();
-  const { nickname } = me.user;
-
-  const send = useCallback(async () => {
-    if (compose.error !== null) {
-      return;
-    }
-    const backupComposeState = compose;
-    const handleRecover = () => {
-      dispatch(makeComposeAction('recoverState', backupComposeState));
-      setBanner(null);
-    };
-    dispatch(makeComposeAction('setSource', { channelId, source: '', range: [0, 0] }));
-
-    const result = await post('/messages/send', null, {
-      messageId: null,
-      previewId: compose.previewId,
-      channelId,
-      name: compose.inputedName.trim() || nickname,
-      text: compose.source,
-      entities: [],
-      inGame: compose.inGame,
-      isAction: false,
-      mediaId: null,
-      pos: null,
-      whisperToUsers: null,
-    });
-    if (result.isOk) {
-      return;
-    }
-    setBanner({
-      level: 'WARNING',
-      content: (
-        <div className="">
-          <FormattedMessage defaultMessage="Error while sending a message, did you recover the message?" />
-          <Button
-            data-small
-            className="ml-2"
-            onClick={handleRecover}
-          >
-            <FormattedMessage defaultMessage="Recover" />
-          </Button>
-        </div>
-      ),
-    });
-  }, [channelId, compose, dispatch, nickname, post, setBanner]);
-
-  useImperativeHandle(ref, () => ({ send }), [send]);
+  const error = useAtomValue(useMemo(() => selectAtom(composeAtom, compose => compose.error), [composeAtom]));
+  const send = useSend(me.user);
   return (
-    <Button onClick={() => send()} disabled={compose.error !== null}>
+    <Button onClick={() => send()} disabled={error !== null}>
       <FormattedMessage defaultMessage="Send" />
       <PaperPlane />
     </Button>
   );
-});
-
-SendButton.displayName = 'SendButton';
+};

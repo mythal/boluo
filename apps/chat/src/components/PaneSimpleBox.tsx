931c39110cb8abd4f8ae9f485c145862937361a0
Tachibana Kiyomi <me@yuru.me>
2023-04-17 07:49:49 +0900

    feat(chat): style fix

--- apps/chat/src/components/PaneSimpleBox.tsx
+++ apps/chat/src/components/PaneSimpleBox.tsx
@@ -6,7 +6,7 @@ interface Props extends ChildrenProps {
 
 export const PaneSimpleBox: FC<Props> = ({ children }) => {
   return (
-    <div className="flex-grow bg-bg flex flex-col max-w-sm">
+    <div className="flex-grow bg-bg flex flex-col">
       <div className="h-pane-header border-b-1/2" />
       <div className="flex-grow flex items-center justify-center">{children}</div>
     </div>
---
+++ apps/chat/src/components/channel/ChatContentFooter.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+interface Props {
+}
+
+export const ChatContentFooter: FC<Props> = () => {
+  return <div className="h-4"></div>;
+};

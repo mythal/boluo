ad9513735cbab1841e1e65cef1613519f3f42671
Tachibana Kiyomi <me@yuru.me>
2023-04-17 17:34:48 +0900

    feat(chat): simple cursor toolbox

---
+++ apps/chat/src/components/channel/OthersCursor.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface Props {
+}
+
+export const OthersCursor: FC<Props> = () => (
+  <span className="inline-block w-2 h-6 absolute bg-highest opacity-40">
+  </span>
+);

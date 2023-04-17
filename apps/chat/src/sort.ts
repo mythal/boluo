0dbb02d500c2fccaf8f84bc6d414fcc4a8b87ea8
Tachibana Kiyomi <me@yuru.me>
2023-04-17 18:19:12 +0900

    refactor(chat): move byPos

--- apps/chat/src/sort.ts
+++ apps/chat/src/sort.ts
@@ -5,3 +5,7 @@ export function by(a: number, b: number): number {
 export function byReverse(a: number, b: number): number {
   return a - b;
 }
+
+export function byPos(a: { pos: number }, b: { pos: number }): number {
+  return a.pos - b.pos;
+}

37600c99580ae1f9f50faf9d858fe8eb817d4547
Tachibana Kiyomi <me@yuru.me>
2023-04-29 19:57:21 +0900

    feat(chat): show delay

--- apps/chat/src/components/sidebar/BaseUrlSelector.tsx
+++ apps/chat/src/components/sidebar/BaseUrlSelector.tsx
@@ -1,7 +1,8 @@
-import { apiUrlAtom } from 'common';
+import { get } from 'api';
+import { apiUrlAtom, useGet } from 'common';
 import { DEFAULT_API_URL } from 'common/hooks/useApiUrl';
 import { useAtom } from 'jotai';
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { Select } from 'ui';
 import { SelectItem } from 'ui/Select';
@@ -18,12 +19,28 @@ export const BaseUrlSelector: FC<Props> = () => {
     { label: 'JP (NNC)', value: 'https://raylet.boluo.chat/api' },
   ], [intl]);
   const [apiUrl, changApiUrl] = useAtom(apiUrlAtom);
+  const [delay, setDelay] = useState<number | 'ERROR' | 'LOADING'>('LOADING');
+  useEffect(() => {
+    const base = new Date().getTime();
+    setDelay('LOADING');
+    get(apiUrl, '/users/get_me', null).then(() => {
+      const now = new Date().getTime();
+      setDelay(now - base);
+    }).catch(() => {
+      setDelay('ERROR');
+    });
+  }, [apiUrl]);
   return (
     <div>
       <label className="flex flex-col gap-1">
         <FormattedMessage defaultMessage="Change Connection Region" />
         <div className="text-surface-900">
           <Select items={items} value={apiUrl} onChange={changApiUrl} />
+        </div>
+        <div>
+          <FormattedMessage defaultMessage="Delay" />: {delay === 'LOADING' && '...'}
+          {delay === 'ERROR' && <FormattedMessage defaultMessage="Failed to connect" />}
+          {typeof delay === 'number' && <span>{delay}</span>}
         </div>
       </label>
     </div>

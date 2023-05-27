f25d209258654dac32189e71b002cdf4a86d8286
Tachibana Kiyomi <me@yuru.me>
2023-05-28 01:05:35 +0900

    feat(chat): improve the style of normal roll

--- apps/chat/src/components/entities/EntityExprRoll.tsx
+++ apps/chat/src/components/entities/EntityExprRoll.tsx
@@ -1,5 +1,6 @@
-import { Dice } from 'icons';
+import { ArrowDownWideShort, ArrowUpWideShort, Dice } from 'icons';
 import { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Roll, RollResult } from '../../interpreter/entities';
 import { Delay } from '../Delay';
 import { FallbackIcon } from '../FallbackIcon';
@@ -9,29 +10,63 @@ interface Props {
   node: Roll | RollResult;
 }
 
+const RollFilter: FC<{ filter: Roll['filter'] | undefined | null }> = ({ filter }) => {
+  if (filter == null) return null;
+  const [type, mount] = filter;
+  return (
+    <span className="mx-0.5 text-sm text-surface-500">
+      (
+      <FormattedMessage defaultMessage="take" />{' '}
+      <Delay fallback={<FallbackIcon />}>
+        {type === 'HIGH' && <ArrowUpWideShort className="inline-block w-[1em] h-[1em]" />}
+        {type === 'LOW' && <ArrowDownWideShort className="inline-block w-[1em] h-[1em]" />}
+      </Delay>{' '}
+      {mount}
+      )
+    </span>
+  );
+};
+
+const Undecided: FC = () => (
+  <span>
+    =??
+  </span>
+);
+
+const ShowRollResult: FC<{ node: RollResult }> = ({ node }) => {
+  if (node.values.length <= 1) {
+    return <span className="ml-0.5">{node.value}</span>;
+  }
+  return (
+    <>
+      =<span className="text-surface-500">[{node.values.join(', ')}]</span>
+      ={node.value}
+    </>
+  );
+};
+
 export const EntityExprRoll: FC<Props> = ({ node }) => {
-  let result: string = '';
+  let result: string = '=??';
   if ('value' in node) {
+    result = '';
     if (node.values.length > 1) {
       result += `=[${node.values.join(', ')}]`;
     }
-    let filter = '';
-    if (node.filter) {
-      const [type, number] = node.filter;
-      filter = ` ${type}(${number}) `;
-    }
-    result += `${filter}=${node.value}`;
+    result += `=${node.value}`;
   }
 
   return (
     <RollBox>
-      <span className="mr-1">
+      <span className="mr-0.5">
         <Delay fallback={<FallbackIcon />}>
           <Dice className="inline-block w-[1em] h-[1em]" />
         </Delay>
       </span>
-      {node.counter}d{node.face}
-      {result}
+      <RollFilter filter={node.filter} />
+      <span className="mx-0.5">
+        {node.counter}d{node.face}
+      </span>
+      {'value' in node ? <ShowRollResult node={node} /> : <Undecided />}
     </RollBox>
   );
 };

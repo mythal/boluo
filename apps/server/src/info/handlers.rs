3dd0aaa5b38c8d3abe915e23302fc27935e17d6d
Tachibana Kiyomi <me@yuru.me>
2023-05-29 01:53:56 +0900

    fix(server): route

--- apps/server/src/info/handlers.rs
+++ apps/server/src/info/handlers.rs
@@ -1,12 +1,13 @@
 use super::models::Proxy;
 use crate::{
     database::{self, Querist},
+    error::AppError,
     interface::Response,
 };
 use hyper::{Body, Method, Request};
 
 // TODO: cache
-pub async fn proxies() -> Response {
+pub async fn proxies() -> Result<Response, AppError> {
     let mut db = database::get().await.expect("Unexpected failture in get database");
     let result = db.query(include_str!("sql/proxies.sql"), &[]).await;
     let proxies: Vec<Proxy> = match result {
@@ -26,11 +27,12 @@ pub async fn proxies() -> Response {
         }
     };
     let body = serde_json::to_string(&proxies).expect("Unexpected failture in serialize proxies");
-    hyper::Response::builder()
+    let response = hyper::Response::builder()
         .header(hyper::header::CONTENT_TYPE, "application/json")
         .status(hyper::StatusCode::OK)
         .body(Body::from(body))
-        .expect("Unexpected failture in build proxies response")
+        .expect("Unexpected failture in build proxies response");
+    Ok(response)
 }
 
 pub fn version() -> Response {
@@ -41,9 +43,9 @@ pub fn version() -> Response {
         .expect("Unexpected failture in build version response")
 }
 
-pub async fn router(req: Request<Body>, path: &str) -> Response {
+pub async fn router(req: Request<Body>, path: &str) -> Result<Response, AppError> {
     match (path, req.method().clone()) {
         ("/proxies", Method::GET) => proxies().await,
-        _ => version(),
+        _ => Ok(version()),
     }
 }

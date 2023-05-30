72fcc0cf3083bbc9d2d4f8cba1cfda844788d2b0
Tachibana Kiyomi <me@yuru.me>
2023-05-31 03:50:31 +0900

    feat(chat): dice pool placeholder

--- apps/chat/src/components/entities/EntityExprDicePool.tsx
+++ apps/chat/src/components/entities/EntityExprDicePool.tsx
@@ -1,5 +1,9 @@
+import { Cubes } from 'icons';
 import { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { DicePool, DicePoolResult } from '../../interpreter/entities';
+import { Delay } from '../Delay';
+import { FallbackIcon } from '../FallbackIcon';
 import { RollBox } from './RollBox';
 
 interface Props {
@@ -9,7 +13,21 @@ interface Props {
 export const EntityExprDicePoolRoll: FC<Props> = ({ node }) => {
   return (
     <RollBox>
-      Dice Pool
+      <Delay fallback={<FallbackIcon />}>
+        <Cubes className="inline" />
+      </Delay>
+      <span className="px-1">
+        <FormattedMessage defaultMessage="Dice Pool" />
+      </span>
+
+      <span className="text-sm italic">
+        <FormattedMessage defaultMessage="Work in progress" />
+      </span>
+      {'value' in node && (
+        <span className="pl-1">
+          ={node.value}
+        </span>
+      )}
     </RollBox>
   );
 };

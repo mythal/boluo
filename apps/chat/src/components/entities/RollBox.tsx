a38a7893ee2f8823b32f662002a918b5585b2f42
Tachibana Kiyomi <me@yuru.me>
2023-05-31 02:36:57 +0900

    feat(chat): style of expression repeat

--- apps/chat/src/components/entities/RollBox.tsx
+++ apps/chat/src/components/entities/RollBox.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const RollBox: FC<Props> = ({ children }) => {
   return (
-    <span className="bg-surface-200 border-surface-300/75 border rounded-sm px-1 py-0.5 shadow-1/2 shadow-surface-800/25">
+    <span className="bg-surface-200 border-surface-300/75 border rounded-sm px-1 py-0.5 shadow-1/2 shadow-surface-500/10">
       {children}
     </span>
   );

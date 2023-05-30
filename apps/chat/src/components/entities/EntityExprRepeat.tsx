3ad33c4b992f36ae20b3d99b430f98635d4ff0e8
Tachibana Kiyomi <me@yuru.me>
2023-05-31 02:36:57 +0900

    feat(chat): style of expression repeat

--- apps/chat/src/components/entities/EntityExprRepeat.tsx
+++ apps/chat/src/components/entities/EntityExprRepeat.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Repeat, RepeatResult } from '../../interpreter/entities';
+import { EvaluatedExprNode, ExprNode, Repeat, RepeatResult } from '../../interpreter/entities';
 import { EntityExprNode } from './EntityExprNode';
 import { EntityExprNodeUnknown } from './EntityExprUnknown';
 
@@ -7,16 +7,32 @@ interface Props {
   node: Repeat | RepeatResult;
 }
 
+export const RepeatItem: FC<{ item: ExprNode | EvaluatedExprNode | 0 }> = ({ item }) => {
+  if (item === 0) {
+    return <span className="px-2">+</span>;
+  }
+  return <EntityExprNode node={item} />;
+};
+
 export const EntityExprRepeat: FC<Props> = ({ node: repeat }) => {
   const { node, count } = repeat;
   if (count === 0) {
     return <EntityExprNodeUnknown />;
   }
-  if ('value' in repeat) {
-    const xs = repeat.evaluated.map((node, key) => <EntityExprNode key={key} node={node} />);
-    return <div className="inline-flex flex-wrap gap-1">[{xs}]={repeat.value}</div>;
-  } else {
-    const xs = Array(count).fill(0).map((_, key) => <EntityExprNode key={key} node={node} />);
-    return <div className="inline-flex flex-wrap gap-1">[{xs}]</div>;
+  const nodeList: Array<0 /* delimiter */ | ExprNode | EvaluatedExprNode> = [];
+  for (let i = 0; i < count; i++) {
+    if ('value' in repeat) {
+      nodeList.push(repeat.evaluated[i] ?? 0);
+    } else {
+      nodeList.push(node);
+    }
+    nodeList.push(0);
   }
+  nodeList.pop();
+  return (
+    <>
+      &#123; {nodeList.map((item, key) => <RepeatItem item={item} key={key} />)}{' '}
+      &#125;{'value' in repeat && <span>={repeat.value}</span>}
+    </>
+  );
 };

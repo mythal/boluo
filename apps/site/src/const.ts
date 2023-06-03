b0dcebcedb0c1bfff852604a5d8a97b6caab8702
Tachibana Kiyomi <me@yuru.me>
2023-06-03 19:57:45 +0900

    feat: better handle BACKEND_URL

--- apps/site/src/const.ts
+++ apps/site/src/const.ts
@@ -1,9 +1,3 @@
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_SAFARI = IS_BROWSER && /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-if (!process.env.BACKEND_URL) {
-  throw new Error('Missing environment variable BACKEND_URL');
-}
-
-export const SERVER_SIDE_API_URL = process.env.BACKEND_URL + '/api';
---
+++ packages/api-browser/src/types.d.ts
@@ -0,0 +1,5 @@
+declare const process: {
+  env: {
+    BACKEND_URL: string | undefined;
+  } | undefined;
+} | undefined;

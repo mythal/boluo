874f23716f1c2401f74951293c80eaec88ed8735
uonr <34709816+uonr@users.noreply.github.com>
2023-07-30 23:14:31 +0900

    feat(chat): let useSpaceInvitationToken do not suspense

--- packages/chat/hooks/useSpaceInvitationToken.ts
+++ packages/chat/hooks/useSpaceInvitationToken.ts
@@ -1,14 +1,12 @@
+import { ApiError } from 'api';
 import { get } from 'api-browser';
-import useSWR from 'swr';
+import useSWR, { SWRResponse } from 'swr';
 import { unwrap } from 'utils';
 
-export const useSpaceInvitationToken = (spaceId: string): string => {
-  const { data } = useSWR(
-    ['/spaces/token' as const, spaceId],
+export const useSpaceInvitationToken = (spaceId: string): SWRResponse<string, ApiError> => {
+  const key = ['/spaces/token', spaceId] as const;
+  return useSWR<string, ApiError, typeof key>(
+    key,
     ([path, id]) => get(path, { id }).then(unwrap),
-    {
-      suspense: true,
-    },
   );
-  return data;
 };

ae2e22fd42cd75800d8fbc67c70c8832e11ce7c0
uonr <34709816+uonr@users.noreply.github.com>
2023-07-31 00:38:02 +0900

    feat(chat): change the icon of ChannelHeaderMoreButton

--- packages/chat/components/pane-channel/ChannelHeaderMenuButton.tsx
+++ packages/chat/components/pane-channel/ChannelHeaderMenuButton.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, Menu } from 'icons';
+import { ChevronDown, Menu, Tool } from 'icons';
 import { FC } from 'react';
 import { Button } from 'ui/Button';
 
@@ -10,8 +10,8 @@ interface Props {
 export const ChannelHeaderMoreButton: FC<Props> = ({ on, toggle }) => {
   return (
     <Button data-small onClick={toggle} data-active={on}>
-      <Menu />
-      <span className="hidden @4xl:inline-flex gap-1 items-center">
+      <Tool />
+      <span className="hidden @xl:inline-flex gap-1 items-center">
         <ChevronDown
           data-on={on}
           className="data-[on=true]:rotate-180 transition-transform duration-200 text-surface-500 data-[on=true]:text-surface-600"

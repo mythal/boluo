cf2b72c6aa454bc6a4faa8b1c69062b7adb970e6
uonr <34709816+uonr@users.noreply.github.com>
2023-07-31 16:17:29 +0900

    feat(chat): always show chevron

--- packages/chat/components/pane-channel/ChannelHeaderFilterButton.tsx
+++ packages/chat/components/pane-channel/ChannelHeaderFilterButton.tsx
@@ -18,12 +18,10 @@ export const ChannelHeaderFilterButton: FC<Props> = ({ on, toggle }) => {
     <div className="relative flex items-stretch">
       <Button data-small onClick={toggle} data-active={on}>
         <Filter />
-        <span className="hidden @4xl:inline-flex gap-1 items-center">
-          <ChevronDown
-            data-on={on}
-            className="data-[on=true]:rotate-180 transition-transform duration-200 text-surface-500 data-[on=true]:text-surface-600"
-          />
-        </span>
+        <ChevronDown
+          data-on={on}
+          className="data-[on=true]:rotate-180 transition-transform duration-200 text-surface-500 data-[on=true]:text-surface-600"
+        />
       </Button>
       {filter !== 'ALL' && <Dot />}
     </div>
--- packages/chat/components/pane-channel/ChannelHeaderMenuButton.tsx
+++ packages/chat/components/pane-channel/ChannelHeaderMenuButton.tsx
@@ -11,12 +11,10 @@ export const ChannelHeaderMoreButton: FC<Props> = ({ on, toggle }) => {
   return (
     <Button data-small onClick={toggle} data-active={on}>
       <Tool />
-      <span className="hidden @xl:inline-flex gap-1 items-center">
-        <ChevronDown
-          data-on={on}
-          className="data-[on=true]:rotate-180 transition-transform duration-200 text-surface-500 data-[on=true]:text-surface-600"
-        />
-      </span>
+      <ChevronDown
+        data-on={on}
+        className="data-[on=true]:rotate-180 transition-transform duration-200 text-surface-500 data-[on=true]:text-surface-600"
+      />
     </Button>
   );
 };

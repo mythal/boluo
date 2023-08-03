9aa49a988f179635291692e0ced8c85027e0f807
uonr <34709816+uonr@users.noreply.github.com>
2023-08-04 03:42:40 +0900

    fix(chat): loading

--- packages/chat/components/ChatSkeleton.tsx
+++ packages/chat/components/ChatSkeleton.tsx
@@ -9,8 +9,8 @@ interface Props {
 export const ChatSkeleton: FC<Props> = ({ children, placeholder }) => {
   return (
     <div className="flex">
-      <div className="flex view-height flex-col flex-none">
-        <div className="h-pane-header border-r border-b-1/2 border-b-gray-400"></div>
+      <div className="flex bg-lowest view-height flex-col flex-none">
+        <div className="h-pane-header border-r "></div>
         <div className="border-r w-sidebar flex-grow"></div>
       </div>
       <PaneSimpleBox>{children || <span className="text-surface-400">{placeholder}</span>}</PaneSimpleBox>

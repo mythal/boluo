84bb941cc3015a702ba0864578cd652fcbe2bcf2
uonr <34709816+uonr@users.noreply.github.com>
2023-08-05 21:41:19 +0900

    fix(chat): switch space

---
+++ packages/chat/hooks/useSwitchSpace.tsx
@@ -0,0 +1,15 @@
+import { useSetAtom } from 'jotai';
+import { useCallback } from 'react';
+import { sidebarContentStateAtom } from '../state/ui.atoms';
+import { panesAtom, routeAtom } from '../state/view.atoms';
+
+export const useSwitchSpace = () => {
+  const setRoute = useSetAtom(routeAtom);
+  const setPanes = useSetAtom(panesAtom);
+  const setSidebarContentState = useSetAtom(sidebarContentStateAtom);
+  return useCallback((spaceId: string) => {
+    setRoute({ type: 'SPACE', spaceId });
+    setPanes([]);
+    setSidebarContentState('CHANNELS');
+  }, [setPanes, setRoute, setSidebarContentState]);
+};

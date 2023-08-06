63ba30b8f0db455be732c8cb1b2cc8878a5f5ce8
uonr <34709816+uonr@users.noreply.github.com>
2023-08-06 17:20:51 +0900

    feat(chat): UX of the SidebarChannelItem

--- packages/chat/components/sidebar/SidebarChannelItem.tsx
+++ packages/chat/components/sidebar/SidebarChannelItem.tsx
@@ -1,7 +1,10 @@
 import type { Channel } from 'api';
 import { Hash } from 'icons';
+import { useSetAtom } from 'jotai';
 import type { FC } from 'react';
+import { usePaneAdd } from '../../hooks/usePaneAdd';
 import { usePaneReplace } from '../../hooks/usePaneReplace';
+import { panesAtom } from '../../state/view.atoms';
 import { SidebarItem } from './SidebarItem';
 
 interface Props {
@@ -11,12 +14,29 @@ interface Props {
 
 export const SidebarChannelItem: FC<Props> = ({ channel, active }) => {
   const replacePane = usePaneReplace();
+  const setPane = useSetAtom(panesAtom);
+  const addPane = usePaneAdd();
   const handleClick = () => {
     replacePane({ type: 'CHANNEL', channelId: channel.id }, (pane) => pane.type === 'CHANNEL');
   };
+  const handleAdd = () => {
+    addPane({ type: 'CHANNEL', channelId: channel.id });
+  };
+  const handleClose = active
+    ? () => {
+      setPane((panes) => panes.filter((pane) => pane.type !== 'CHANNEL' || pane.channelId !== channel.id));
+    }
+    : undefined;
 
   return (
-    <SidebarItem onClick={handleClick} icon={<Hash />} active={active}>
+    <SidebarItem
+      onClick={handleClick}
+      icon={<Hash />}
+      active={active}
+      onClose={handleClose}
+      onAdd={handleAdd}
+      varant="flat"
+    >
       <span className="text-left break-all">
         {channel.name}
       </span>
--- packages/chat/components/sidebar/SidebarItem.tsx
+++ packages/chat/components/sidebar/SidebarItem.tsx
@@ -1,25 +1,81 @@
 import clsx from 'clsx';
-import type { FC, ReactNode } from 'react';
+import { X } from 'icons';
+import { FC, ReactNode, useMemo } from 'react';
 import { useSidebarState } from './useSidebarState';
 
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
+  onAdd?: () => void;
+  onClose?: () => void;
+  varant?: 'default' | 'flat';
   icon?: ReactNode;
   active?: boolean;
   toggle?: boolean;
   children?: ReactNode;
 }
 
+const SidebarItemExtraButton: FC<{ onClick: () => void; children: ReactNode }> = ({ onClick, children }) => (
+  <button
+    onClick={(e) => {
+      e.stopPropagation();
+      onClick();
+    }}
+    className={clsx(
+      'transition-opacity duration-100 ease-out opacity-50 group-hover:opacity-100',
+      'flex-none inline-flex items-center justify-center',
+      'w-6 h-6 rounded-sm border border-surface-50 text-center group-hover:border-surface-200',
+      'text-surface-400  bg-lowest hover:border-surface-400 hover:text-surface-600',
+    )}
+  >
+    {children}
+  </button>
+);
+
 export const SidebarItem: FC<Props> = (
-  { onClick, icon, children, active = false, toggle = false },
+  { onClick, onClose, onAdd, icon, children, active = false, toggle = false, varant = 'default' },
 ) => {
   const { isExpanded } = useSidebarState();
+  const extraNode = useMemo(() => {
+    if (toggle) {
+      return (
+        <span
+          className={clsx(
+            'inline-block w-4 text-center text-surface-400 group-hover:text-brand-500',
+            isExpanded ? '' : 'hidden',
+          )}
+        >
+          {active ? '×' : '•'}
+        </span>
+      );
+    }
+    if (onClose) {
+      return (
+        <SidebarItemExtraButton onClick={onClose}>
+          <span className="transition-transform transform duration-100">
+            <X />
+          </span>
+        </SidebarItemExtraButton>
+      );
+    }
+    if (onAdd) {
+      return (
+        <SidebarItemExtraButton onClick={onAdd}>
+          <span className="transition-transform duration-100 transform rotate-45">
+            <X />
+          </span>
+        </SidebarItemExtraButton>
+      );
+    }
+    return null;
+  }, [active, isExpanded, onAdd, onClose, toggle]);
   return (
     <button
       onClick={onClick}
       className={clsx(
-        'group flex text-left items-center justify-center gap-1 w-full hover:bg-surface-100',
-        isExpanded ? 'text-left text-sm  px-4 py-3' : 'text-center justify-center text-lg px-2 py-3',
+        'group flex text-left items-center justify-center gap-1 w-full min-h-[2rem] relative',
+        onClick ? 'cursor-pointer hover:bg-surface-100' : 'cursor-default',
+        isExpanded ? 'text-left text-sm  px-4' : 'text-center justify-center text-lg px-2',
+        varant === 'flat' ? 'py-2' : 'py-3',
         active && 'bg-surface-50',
       )}
     >
@@ -28,11 +84,7 @@ export const SidebarItem: FC<Props> = (
         {isExpanded && children}
       </span>
 
-      {toggle && (
-        <span className={clsx(isExpanded ? '' : 'hidden', active ? 'text-brand-500' : 'text-surface-300')}>
-          {active ? '•' : '•'}
-        </span>
-      )}
+      {extraNode}
     </button>
   );
 };

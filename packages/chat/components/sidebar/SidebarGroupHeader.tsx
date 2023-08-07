d28fb00740d4963bfb82665aa6f11e40796945fd
uonr <34709816+uonr@users.noreply.github.com>
2023-08-07 12:14:07 +0900

    feat(chat): adjust the style of `SidebarGroupHeader`

--- packages/chat/components/sidebar/SidebarGroupHeader.tsx
+++ packages/chat/components/sidebar/SidebarGroupHeader.tsx
@@ -14,7 +14,7 @@ export const SidebarGroupHeader: FC<Props> = ({ folded, toggle, children, disabl
   return (
     <button
       onClick={toggle}
-      className="flex gap-2 items-center justify-between w-full text-surface-600 py-3 px-3 border-surface-100 group cursor-pointer hover:bg-surface-100"
+      className="flex gap-2 items-center justify-between w-full text-surface-600 py-2 px-3 border-surface-100 group cursor-pointer"
     >
       <div className="flex text-left gap-2 min-w-0 text-base">
         {children}
@@ -22,8 +22,8 @@ export const SidebarGroupHeader: FC<Props> = ({ folded, toggle, children, disabl
       {!disabled && (
         <span
           className={clsx(
-            'w-8 h-8 flex-none inline-flex items-center justify-center border rounded-md bg-surface-50 text-sm',
-            folded ? 'group-hover:border-surface-300' : 'border-surface-400',
+            'w-8 h-8 flex-none inline-flex items-center justify-center border rounded-md bg-surface-50 text-sm group-hover:border-surface-500 group-hover:bg-surface-100',
+            folded ? '' : 'border-surface-400',
           )}
         >
           {Icon == null ? (folded ? <ChevronDown /> : <ChevronUp />) : <Icon />}

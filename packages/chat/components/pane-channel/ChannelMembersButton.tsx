9eda40cfc092a6232037e221cc86e58180a6ff13
uonr <34709816+uonr@users.noreply.github.com>
2023-08-10 04:29:50 +0900

    feat(chat): channel member list state

--- packages/chat/components/pane-channel/ChannelMembersButton.tsx
+++ packages/chat/components/pane-channel/ChannelMembersButton.tsx
@@ -1,7 +1,9 @@
 import { Channel } from 'api';
 import { Users } from 'icons';
+import { useAtom } from 'jotai';
 import { FC, useMemo } from 'react';
 import { Button } from 'ui/Button';
+import { useChannelAtoms } from '../../hooks/useChannelAtoms';
 import { useQueryChannelMembers } from '../../hooks/useQueryChannelMembers';
 
 interface Props {
@@ -9,6 +11,8 @@ interface Props {
 }
 
 export const ChannelMembersButton: FC<Props> = ({ channelId }) => {
+  const { memberListStateAtom } = useChannelAtoms();
+  const [memberListState, setMemberListState] = useAtom(memberListStateAtom);
   const { data: membersInfo, isLoading } = useQueryChannelMembers(channelId);
   const onlineCount = useMemo(() => {
     if (membersInfo == null) return 0;
@@ -16,11 +20,16 @@ export const ChannelMembersButton: FC<Props> = ({ channelId }) => {
     return 0;
   }, [membersInfo]);
   return (
-    <Button data-small disabled={isLoading}>
+    <Button
+      data-small
+      data-active={memberListState !== 'CLOSED'}
+      disabled={isLoading}
+      onClick={() => setMemberListState(prevState => prevState !== 'CLOSED' ? 'CLOSED' : 'RIGHT')}
+    >
       <Users />
       {membersInfo != null && (
-        <span className="hidden @lg:inline">
-          {onlineCount}
+        <span className="hidden @xl:inline">
+          {onlineCount}/{membersInfo.members.length}
         </span>
       )}
     </Button>

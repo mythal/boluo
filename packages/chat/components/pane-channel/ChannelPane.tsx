aca4a79b3ad29c4641e3174f775ae5b6856b8fe6
uonr <34709816+uonr@users.noreply.github.com>
2023-08-10 11:40:20 +0900

    feat(chat): simple member list

--- packages/chat/components/pane-channel/ChannelPane.tsx
+++ packages/chat/components/pane-channel/ChannelPane.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { useMe } from 'common';
-import { Lock } from 'icons';
-import { atom } from 'jotai';
+import { Lock, LockedHash } from 'icons';
+import { atom, useAtomValue } from 'jotai';
 import { atomWithReducer, atomWithStorage } from 'jotai/utils';
 import { FC, useMemo } from 'react';
 import { memo } from 'react';
@@ -12,10 +12,13 @@ import { useQueryChannel } from '../../hooks/useQueryChannel';
 import { composeReducer, makeInitialComposeState } from '../../state/compose.reducer';
 import { Compose } from '../compose/Compose';
 import { useSendPreview } from '../compose/useSendPreview';
+import { ErrorDisplay } from '../ErrorDisplay';
 import { PaneBox } from '../PaneBox';
+import { PaneHeaderBox } from '../PaneHeaderBox';
 import { PaneLoading } from '../PaneLoading';
 import { ChannelHeader } from './ChannelHeader';
 import { ChatContent } from './ChatContent';
+import { MemberList } from './MemberList';
 
 interface Props {
   channelId: string;
@@ -45,20 +48,45 @@ export const ChatPaneChannel: FC<Props> = memo(({ channelId }) => {
     memberListStateAtom: atom<ChannelMemberListState>('CLOSED'),
   }), [channelId, initialCompose]);
   const nickname = me != null && me !== 'LOADING' ? me.user.nickname : undefined;
-  const { data: channel, isLoading } = useQueryChannel(channelId);
+  const { data: channel, isLoading, error } = useQueryChannel(channelId);
   useSendPreview(channelId, nickname, atoms.composeAtom);
-  if (isLoading || member === 'LOADING') {
+  const memberListState = useAtomValue(atoms.memberListStateAtom);
+  if (error) {
+    return (
+      <div className="p-4">
+        <ErrorDisplay error={error} />
+      </div>
+    );
+  }
+  if (channel == null || (!channel.isPublic && member === 'LOADING')) {
     return <PaneLoading />;
+  }
+  if (!channel.isPublic && member == null) {
+    return (
+      <PaneBox
+        header={
+          <PaneHeaderBox icon={<LockedHash />}>
+            <FormattedMessage defaultMessage="Secret Channel" />
+          </PaneHeaderBox>
+        }
+      >
+        <SecretChannelInfo className="p-4" />
+      </PaneBox>
+    );
   }
   return (
     <ChannelAtomsContext.Provider value={atoms}>
       <PaneBox header={<ChannelHeader />}>
-        <div className={clsx('flex-col justify-between flex flex-grow relative h-full')}>
-          {member == null && channel != null && !channel.isPublic
-            ? <SecretChannelInfo className="p-4" />
-            : <ChatContent className="relative flex-grow flex-[1_1_100%]" me={me} channelId={channelId} />}
+        <div
+          className={clsx(
+            'grid grid-rows-[1fr_auto] relative h-full',
+            memberListState === 'CLOSED' ? 'grid-cols-1' : 'grid-cols-[1fr_auto]',
+          )}
+        >
+          <ChatContent className="relative" me={me} channelId={channelId} />
+          {memberListState === 'RIGHT' && <MemberList className="max-w-[12rem]" channelId={channelId} />}
           {me && me !== 'LOADING' && member
-            ? <Compose me={me} className={clsx('p-2 border-t flex-[1_0_auto]')} />
+            ? <Compose me={me} className={clsx('p-2 border-t col-span-full')} />
             : null}
         </div>
       </PaneBox>

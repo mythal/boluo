adaffbd464c81be485a83dac31aa772d0d5a0ae7
uonr <34709816+uonr@users.noreply.github.com>
2023-08-14 16:07:02 +0900

    fix(chat): logout in firefox

--- packages/chat/hooks/useLogout.ts
+++ packages/chat/hooks/useLogout.ts
@@ -2,23 +2,10 @@ import { get } from 'api-browser';
 import { useCallback } from 'react';
 import { useSWRConfig } from 'swr';
 
-async function deleteAllIndexedDbDatabases() {
-  // https://gist.github.com/rmehner/b9a41d9f659c9b1c3340
-  const dbs = await window.indexedDB.databases();
-  dbs.forEach(db => {
-    if (db.name) {
-      window.indexedDB.deleteDatabase(db.name);
-    }
-  });
-}
-
 export function useLogout(): () => void {
   const { mutate } = useSWRConfig();
   return useCallback(async () => {
-    await Promise.all([
-      get('/users/logout', null),
-      deleteAllIndexedDbDatabases(),
-    ]);
+    await get('/users/logout', null);
     localStorage.clear();
     sessionStorage.clear();
     await mutate(['/users/get_me'], null);

b16c314fcb1663821547b3fae412a70ced88f74a
uonr <34709816+uonr@users.noreply.github.com>
2023-08-16 23:41:13 +0900

    feat(chat): simple name history

--- packages/chat/components/pane-channel/SelfPreviewNameCell.tsx
+++ packages/chat/components/pane-channel/SelfPreviewNameCell.tsx
@@ -1,7 +1,13 @@
 import { ChannelMember } from 'api';
-import { FC, memo, ReactNode } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { History } from 'icons';
+import { useSetAtom, useStore } from 'jotai';
+import { FC, memo, useMemo } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import Icon from 'ui/Icon';
+import { useComposeAtom } from '../../hooks/useComposeAtom';
 import { useEditChannelCharacterName } from '../../hooks/useEditChannelCharacterName';
+import { chatAtom } from '../../state/chat.atoms';
+import { ChatSpaceState } from '../../state/chat.reducer';
 import { InGameSwitchButton } from '../compose/InGameSwitchButton';
 import { IsActionIndicator } from './IsActionIndicator';
 import { Name } from './Name';
@@ -20,21 +26,70 @@ const SaveAsCharacterName: FC<{ channelId: string; characterName: string }> = ({
     isMutating,
   } = useEditChannelCharacterName(channelId);
   return (
-    <div className="absolute left-0 bottom-full w-max bg-highest/60 text-brand-300 hover:text-brand-200 rounded shadow px-2 py-1 -translate-x-2">
+    <div className="absolute left-0 bottom-full w-max bg-highest/60  rounded shadow px-2 py-1 -translate-x-2">
       {isMutating
         ? (
-          <span className="text-sm animate-pulse">
+          <span className="text-sm animate-pulse text-lowest">
             Saving...
           </span>
         )
         : (
           <button
-            className="text-sm underline flex-none"
+            className="text-sm underline flex-none text-brand-300 hover:text-brand-200"
             onClick={() => trigger({ characterName })}
           >
             <FormattedMessage defaultMessage="As default character name" />
           </button>
         )}
+    </div>
+  );
+};
+
+const chatStateToNameList = (state: ChatSpaceState, channelId: string, myId: string): string[] => {
+  const channelState = state.channels[channelId];
+  if (channelState == null) return [];
+  const names: string[] = [];
+
+  for (let i = channelState.messages.length - 1; i >= 0; i--) {
+    const message = channelState.messages[i]!;
+    if (!message.inGame || message.senderId !== myId || names.includes(message.name)) continue;
+    names.push(message.name);
+    if (names.length > 4) return names;
+  }
+  return names;
+};
+
+const NameHistory: FC<{ channelId: string; myId: string }> = ({ channelId, myId }) => {
+  const intl = useIntl();
+  const store = useStore();
+  const title = intl.formatMessage({ defaultMessage: 'Name History' });
+  const nameHistory = useMemo(
+    // In this case, we don't need to use atom
+    () => chatStateToNameList(store.get(chatAtom), channelId, myId),
+    [channelId, myId, store],
+  );
+
+  const dispatch = useSetAtom(useComposeAtom());
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch({ type: 'setInputedName', payload: { inputedName: e.target.value } });
+  };
+  if (nameHistory.length === 0) {
+    return null;
+  }
+  return (
+    <div className="w-8 relative inline-block flex-none">
+      <select
+        value={''}
+        title={title}
+        className="border rounded w-full h-full text-sm appearance-none outline-none text-transprent"
+        onChange={handleChange}
+      >
+        <option value="">
+          <FormattedMessage defaultMessage="Name History" />
+        </option>
+        {nameHistory.map((name, key) => <option key={key} value={name}>{name}</option>)}
+      </select>
+      <Icon className="absolute left-0 translate-x-1/2 pointer-events-none top-0 translate-y-1/2" icon={History} />
     </div>
   );
 };
@@ -53,6 +108,8 @@ export const SelfPreviewNameCell = memo<Props>(({ inGame, name, isAction, channe
 
       <div className="flex-shrink flex gap-1 h-8">
         {inGame && <NameInput className="text-sm w-[5rem] @xs:w-[7rem] @2xl:w-full " />}
+
+        {inGame && <NameHistory myId={channelMember.userId} channelId={channelMember.channelId} />}
         <InGameSwitchButton type="ICON" />
       </div>
     </div>

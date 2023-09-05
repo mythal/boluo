35628557146140328686f3bb4f6fab8a7bdc28ec
uonr <34709816+uonr@users.noreply.github.com>
2023-09-05 19:09:22 +0900

    feat: breakpoint

---
+++ packages/chat/breakpoint.tsx
@@ -0,0 +1,65 @@
+import React, { FC, ReactNode, useContext } from 'react';
+import { useEffect, useState } from 'react';
+import screens from 'ui/screens.json';
+
+type Screens = typeof screens;
+
+export type Breakpoint = keyof Screens | 'xs';
+
+export const compareBreakpoint = (a: Breakpoint, b: Breakpoint): number => {
+  const screensEntries = Object.entries(screens);
+  screensEntries.sort(([, a], [, b]) => b - a);
+
+  const aIndex = screensEntries.findIndex(([key]) => key === a);
+  const bIndex = screensEntries.findIndex(([key]) => key === b);
+
+  return aIndex - bIndex;
+};
+
+const windowBreakpoint = (): Breakpoint => {
+  const { innerWidth } = window;
+
+  const screensEntries = Object.entries(screens);
+  screensEntries.sort(([, a], [, b]) => b - a);
+
+  for (const [key, value] of screensEntries) {
+    if (innerWidth >= value) {
+      return key as Breakpoint;
+    }
+  }
+  return 'xs';
+};
+
+const useListenBreakpoint = (): Breakpoint => {
+  const [breakpoint, setBreakpoint] = useState<Breakpoint>(windowBreakpoint());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBreakpoint(windowBreakpoint);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return breakpoint;
+};
+
+const BreakpointContext = React.createContext<Breakpoint | null>(null);
+
+export const BreakpointProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const breakpoint = useListenBreakpoint();
+
+  return <BreakpointContext.Provider value={breakpoint}>{children}</BreakpointContext.Provider>;
+};
+
+export const useBreakpoint = (): Breakpoint => {
+  const breakpoint = useContext(BreakpointContext);
+  if (breakpoint === null) {
+    return windowBreakpoint();
+  } else {
+    return breakpoint;
+  }
+};

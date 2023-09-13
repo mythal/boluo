16e0e04b7ed4d58c25741337052547577ed3357e
uonr <34709816+uonr@users.noreply.github.com>
2023-09-13 20:58:47 +0800

    feat(chat): filter style

--- packages/chat/components/pane-channel/ChannelHeaderFilterBar.tsx
+++ packages/chat/components/pane-channel/ChannelHeaderFilterBar.tsx
@@ -6,7 +6,7 @@ interface Props {}
 
 export const ChannelHeaderFilterBar: FC<Props> = () => {
   return (
-    <div className="py-2 px-4 border-b flex justify-between items-center gap-2">
+    <div className="py-2 px-4 border-b flex flex-col @md:flex-row justify-between @md:items-center gap-2">
       <ChannelHeaderFilter />
       <ChannelHeaderFilterShowArchive />
     </div>

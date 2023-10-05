a35d3e25f2994f5129be0b5c53086e9a1e9d024a
uonr <34709816+uonr@users.noreply.github.com>
2023-10-06 08:35:13 +0900

    feat: name toolbox

--- packages/chat/components/pane-channel/SelfPreviewNameCell.tsx
+++ packages/chat/components/pane-channel/SelfPreviewNameCell.tsx
@@ -1,18 +1,14 @@
 import { ChannelMember } from 'api';
 import clsx from 'clsx';
-import { History, Save } from 'icons';
-import { useSetAtom, useStore } from 'jotai';
-import { FC, memo, useMemo } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { Save } from 'icons';
+import { FC, memo } from 'react';
+import { FormattedMessage } from 'react-intl';
 import Icon from 'ui/Icon';
-import { useComposeAtom } from '../../hooks/useComposeAtom';
 import { useEditChannelCharacterName } from '../../hooks/useEditChannelCharacterName';
-import { chatAtom } from '../../state/chat.atoms';
-import { ChatSpaceState } from '../../state/chat.reducer';
-import { InGameSwitchButton } from '../compose/InGameSwitchButton';
+import { Delay } from '../Delay';
 import { IsActionIndicator } from './IsActionIndicator';
 import { Name } from './Name';
-import { NameInput } from './NameInput';
+import { NameToolbox } from './NameToolbox';
 
 interface Props {
   inGame: boolean;
@@ -50,72 +46,19 @@ const SaveAsCharacterName: FC<{ channelId: string; characterName: string }> = ({
   );
 };
 
-const chatStateToNameList = (state: ChatSpaceState, channelId: string, myId: string): string[] => {
-  const channelState = state.channels[channelId];
-  if (channelState == null) return [];
-  const names: string[] = [];
-
-  for (let i = channelState.messages.length - 1; i >= 0; i--) {
-    const message = channelState.messages[i]!;
-    if (!message.inGame || message.senderId !== myId || names.includes(message.name)) continue;
-    names.push(message.name);
-    if (names.length > 4) return names;
-  }
-  return names;
-};
-
-const NameHistory: FC<{ channelId: string; myId: string }> = ({ channelId, myId }) => {
-  const intl = useIntl();
-  const store = useStore();
-  const title = intl.formatMessage({ defaultMessage: 'Name History' });
-  const nameHistory = useMemo(
-    // In this case, we don't need to use atom
-    () => chatStateToNameList(store.get(chatAtom), channelId, myId),
-    [channelId, myId, store],
-  );
-
-  const dispatch = useSetAtom(useComposeAtom());
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch({ type: 'setInputedName', payload: { inputedName: e.target.value } });
-  };
-  if (nameHistory.length === 0) {
-    return null;
-  }
-  return (
-    <div className="w-8 relative inline-block flex-none">
-      <select
-        value={''}
-        title={title}
-        className="border rounded bg-surface-300 hover:bg-surface-200 w-full h-full text-sm appearance-none outline-none text-transprent"
-        onChange={handleChange}
-      >
-        <option value="">
-          <FormattedMessage defaultMessage="Name History" />
-        </option>
-        {nameHistory.map((name, key) => <option key={key} value={name}>{name}</option>)}
-      </select>
-      <Icon className="absolute left-0 translate-x-1/2 pointer-events-none top-0 translate-y-1/2" icon={History} />
-    </div>
-  );
-};
-
 export const SelfPreviewNameCell = memo<Props>(({ inGame, name, isAction, channelMember }) => {
   const { isMaster, characterName } = channelMember;
   return (
     <div className="flex @2xl:flex-col pb-2 gap-y-1 gap-x-4 items-center @2xl:items-end justify-between @2xl:justify-start">
       <div className="flex-grow flex-shrink-1 @2xl:flex-shrink-0 max-w-full relative">
         {!isAction ? <Name name={name} isMaster={isMaster} isPreview self /> : <IsActionIndicator />}
+        <NameToolbox channelMember={channelMember} />
 
         {inGame && name !== '' && characterName !== name && (
-          <SaveAsCharacterName channelId={channelMember.channelId} characterName={name} />
+          <Delay fallback={null}>
+            <SaveAsCharacterName channelId={channelMember.channelId} characterName={name} />
+          </Delay>
         )}
-      </div>
-
-      <div className="flex-shrink flex gap-1 h-8">
-        {inGame && <NameInput className="text-sm w-[5rem] @xs:w-[7rem] @2xl:w-full " />}
-
-        {inGame && <NameHistory myId={channelMember.userId} channelId={channelMember.channelId} />}
-        <InGameSwitchButton type="ICON" />
       </div>
     </div>
   );

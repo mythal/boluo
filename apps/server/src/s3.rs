8e770f925aa4b92378e095b6b96ac83f60cd93cb
uonr <dev@yuru.me>
2023-12-26 12:11:36 +0900

    refactor(devcontainer): network

--- apps/server/src/s3.rs
+++ apps/server/src/s3.rs
@@ -1,8 +1,9 @@
 use std::sync::OnceLock;
 
 use aws_config::SdkConfig;
+use aws_sdk_s3::Config;
 
-fn make_config() -> SdkConfig {
+fn make_config() -> Config {
     use aws_credential_types::Credentials;
     use std::env;
     let access_key = env::var("S3_ACCESS_KEY_ID").expect("S3_ACCESS_KEY is not set");
@@ -11,21 +12,24 @@ fn make_config() -> SdkConfig {
 
     let credentials = Credentials::new(access_key, secret_access_key, None, None, "boluo");
     let credentials_provider = aws_credential_types::provider::SharedCredentialsProvider::new(credentials);
-    SdkConfig::builder()
+    Config::builder()
+        .behavior_version_latest()
+        // Fix DNS resolution error
+        .force_path_style(true)
         .region(aws_sdk_s3::config::Region::new("auto"))
         .endpoint_url(endpoint_url)
         .credentials_provider(credentials_provider)
         .build()
 }
 
-fn get_config() -> &'static SdkConfig {
-    static SDK_CONFIG: OnceLock<SdkConfig> = OnceLock::new();
+fn get_config() -> &'static Config {
+    static SDK_CONFIG: OnceLock<Config> = OnceLock::new();
     SDK_CONFIG.get_or_init(make_config)
 }
 
 pub fn get_client() -> &'static aws_sdk_s3::Client {
     static CLIENT: OnceLock<aws_sdk_s3::Client> = OnceLock::new();
-    CLIENT.get_or_init(|| aws_sdk_s3::Client::new(get_config()))
+    CLIENT.get_or_init(|| aws_sdk_s3::Client::from_conf(make_config()))
 }
 
 pub fn get_bucket_name() -> &'static str {

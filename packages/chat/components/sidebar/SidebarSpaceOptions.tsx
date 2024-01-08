09ab985cbba94d10b347b9b8ad57112c24c748c5
uonr <dev@yuru.me>
2024-01-08 14:10:07 +0900

    feat(chat): simplify sidebar header

--- packages/chat/components/sidebar/SidebarSpaceOptions.tsx
+++ packages/chat/components/sidebar/SidebarSpaceOptions.tsx
@@ -1,39 +1,21 @@
 import type { Space } from 'api';
-import { useMe } from 'common';
-import { Portal, Settings, Shuffle, Tool, Users } from 'icons';
-import { useAtom, useAtomValue, useSetAtom } from 'jotai';
+import { Shuffle } from 'icons';
+import { useAtom } from 'jotai';
 import type { FC } from 'react';
-import { useState } from 'react';
-import { useMemo } from 'react';
-import { FormattedMessage } from 'react-intl';
 import { usePaneToggle } from '../../hooks/usePaneToggle';
-import { useMySpaceMember } from '../../hooks/useQueryMySpaceMember';
 import { sidebarContentStateAtom } from '../../state/ui.atoms';
-import { panesAtom } from '../../state/view.atoms';
-import { SpaceMembersPane, SpaceSettingsPane } from '../../state/view.types';
-import { SidebarGroupHeader } from './SidebarGroupHeader';
-import { SidebarItem } from './SidebarItem';
+import Icon from 'ui/Icon';
+import clsx from 'clsx';
 
 interface Props {
   space: Space;
 }
 
 export const SpaceOptions: FC<Props> = ({ space }) => {
-  const panes = useAtomValue(panesAtom);
-  const me = useMe();
-  const { data: mySpaceMember } = useMySpaceMember(space.id);
-  const disabled = me === null;
-  const [folded, setFold] = useState(true);
   const togglePane = usePaneToggle();
-  const spaceSettingsPane: SpaceSettingsPane = { type: 'SPACE_SETTINGS', spaceId: space.id };
-  const spaceMembersPane: SpaceMembersPane = { type: 'SPACE_MEMBERS', spaceId: space.id };
-  const spaceSettingsActive = useMemo(() => panes.findIndex((pane) => pane.type === 'SPACE_SETTINGS') !== -1, [panes]);
-  const spaceMembersActive = useMemo(() => panes.findIndex((pane) => pane.type === 'SPACE_MEMBERS') !== -1, [panes]);
   const [sidebarState, setSidebarState] = useAtom(sidebarContentStateAtom);
-  const handleToggle = () => {
-    if (!disabled) {
-      setFold((folded) => !folded);
-    }
+  const handleToggleSpacePane = () => {
+    togglePane({ type: 'SPACE', spaceId: space.id });
   };
 
   const handleClickSwitchSpace = () => {
@@ -41,32 +23,20 @@ export const SpaceOptions: FC<Props> = ({ space }) => {
   };
   return (
     <div className="">
-      <SidebarGroupHeader disabled={disabled} folded={folded} toggle={handleToggle} icon={Tool}>
-        <div className="break-all">{space.name}</div>
-      </SidebarGroupHeader>
-
-      {!folded && (
-        <div className="pb-2">
-          {mySpaceMember?.isAdmin && (
-            <SidebarItem
-              icon={<Settings />}
-              active={spaceSettingsActive}
-              onClick={() => togglePane(spaceSettingsPane)}
-              toggle
-            >
-              <FormattedMessage defaultMessage="Space Settings" />
-            </SidebarItem>
+      <div className="group flex w-full items-center gap-2 px-3 py-2">
+        <button className="min-w-0 flex-grow items-center gap-2 text-left text-base " onClick={handleToggleSpacePane}>
+          {space.name}
+        </button>
+        <button
+          className={clsx(
+            'h-8 w-8 rounded-md border',
+            sidebarState === 'SPACES' ? 'border-surface-500' : 'hover:border-surface-300',
           )}
-
-          <SidebarItem icon={<Users />} active={spaceMembersActive} onClick={() => togglePane(spaceMembersPane)} toggle>
-            <FormattedMessage defaultMessage="Members" />
-          </SidebarItem>
-
-          <SidebarItem icon={<Shuffle />} onClick={handleClickSwitchSpace} active={sidebarState === 'SPACES'} toggle>
-            <FormattedMessage defaultMessage="Switch Space" />
-          </SidebarItem>
-        </div>
-      )}
+          onClick={handleClickSwitchSpace}
+        >
+          <Icon icon={Shuffle} />
+        </button>
+      </div>
     </div>
   );
 };

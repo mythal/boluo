3acf7a87b8c07696342949b43e3dd7e6441488b7
uonr <dev@yuru.me>
2024-01-08 12:20:47 +0900

    feat(chat): sidebar toggler style

--- packages/chat/components/sidebar/ToggleSidebarLine.tsx
+++ packages/chat/components/sidebar/ToggleSidebarLine.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
-import { Sidebar } from 'icons';
+import { ChevronLeft, ChevronRight, Sidebar } from 'icons';
 import { useAtom } from 'jotai';
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, MouseEventHandler, useCallback, useEffect, useRef, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import Icon from 'ui/Icon';
 import { isSidebarExpandedAtom } from '../../state/ui.atoms';
@@ -10,8 +10,10 @@ interface Props {}
 
 export const ToggleSidebarLine: FC<Props> = () => {
   const [isExpanded, setExpand] = useAtom(isSidebarExpandedAtom);
+  const isExpandedRef = useRef(isExpanded);
+  isExpandedRef.current = isExpanded;
   const intl = useIntl();
-  const title = intl.formatMessage({ defaultMessage: 'Open Sidebar' });
+  const title = intl.formatMessage({ defaultMessage: 'Toggle Sidebar' });
   const [dragging, setDragging] = useState(false);
   const draggingRef = useRef(dragging);
   draggingRef.current = dragging;
@@ -19,6 +21,7 @@ export const ToggleSidebarLine: FC<Props> = () => {
     setDragging(true);
   };
   const toggleSidebar = useCallback(() => setExpand((x) => !x), [setExpand]);
+  const buttonRef = useRef<HTMLButtonElement>(null);
   useEffect(() => {
     const listener = () => {
       if (draggingRef.current) {
@@ -29,20 +32,52 @@ export const ToggleSidebarLine: FC<Props> = () => {
     window.addEventListener('mouseup', listener);
     return () => window.removeEventListener('mouseup', listener);
   }, [toggleSidebar]);
+
+  const previousTriggerTime = useRef(new Date().getTime());
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (buttonRef.current == null) return;
+      const now = new Date().getTime();
+      if (now - previousTriggerTime.current < 16) return;
+      const NEAR_DISTANCE = 100;
+      const near = buttonRef.current.getAttribute('data-near') === 'true';
+      let distance = e.clientX;
+      if (isExpandedRef.current) {
+        const x = buttonRef.current.getBoundingClientRect().x;
+        distance = Math.abs(e.clientX - x);
+      }
+      if (near && distance > NEAR_DISTANCE) {
+        buttonRef.current.removeAttribute('data-near');
+      } else if (!near && distance < NEAR_DISTANCE) {
+        buttonRef.current.setAttribute('data-near', 'true');
+      }
+    };
+    document.addEventListener('mousemove', listener, { passive: true });
+    return () => document.removeEventListener('mousemove', listener);
+  }, []);
   return (
     <button
-      className="group absolute left-0 top-0 z-20 h-full w-3 cursor-col-resize"
+      ref={buttonRef}
+      className="group absolute left-0 top-0 z-20 h-full w-2 cursor-pointer hover:w-5 hover:bg-blue-400/15 data-[near=true]:bg-blue-300/10"
       onMouseDown={handleMouseDown}
-      onClick={toggleSidebar}
       title={title}
     >
+      <div
+        className={clsx(
+          'bg-lowest group-hover:border-transprent absolute z-30 box-content text-sm',
+          'h-[20px] w-[18px] -translate-x-[2px] -translate-y-[2px] cursor-pointer',
+          'shadow-surface-300 rounded-br-lg shadow-[0_0_0_1px] group-hover:shadow-[0_0_0_2px] group-hover:shadow-blue-300',
+        )}
+      >
+        <Icon icon={isExpanded ? ChevronLeft : ChevronRight} />
+      </div>
       <div
         className={clsx(
           'bg-surface-300 h-full w-[1px] group-hover:w-[2px] group-hover:bg-blue-300',
           dragging && 'bg-blue-500',
         )}
       ></div>
-      <div className="bg-highest text-lowest absolute left-2 top-2 z-30 hidden w-max space-x-1 rounded-sm px-2 py-1 group-hover:block">
+      <div className="bg-highest text-lowest absolute left-4 top-8 z-30 hidden w-max space-x-1 rounded-sm px-2 py-1 group-hover:block">
         <span>
           <Icon icon={Sidebar} />
         </span>

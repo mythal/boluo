a67343d58a48f52bfc2ff9354315bd76b76820d5
uonr <dev@yuru.me>
2024-01-16 20:41:46 +0900

    feat(chat): redesign `SelfCursorToolbar`

--- packages/chat/components/pane-channel/Content.tsx
+++ packages/chat/components/pane-channel/Content.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import Prando from 'prando';
-import { memo, ReactNode, useMemo, useRef } from 'react';
+import { memo, ReactNode, RefObject, useEffect, useMemo, useRef } from 'react';
 import type { Entity } from '../../interpreter/entities';
 import { makeRng } from '../../interpreter/eval';
 import { Delay } from '../Delay';
@@ -12,7 +12,7 @@ import { EntityExpr } from '../entities/EntityExpr';
 import { EntityLink } from '../entities/EntityLink';
 import { EntityStrong } from '../entities/EntityStrong';
 import { EntityText } from '../entities/EntityText';
-import { SelfCursorToolbar } from './SelfCursorToolbar';
+import { CursorToolbarHandle, SelfCursorToolbar } from './SelfCursorToolbar';
 
 interface Props {
   channelId: string;
@@ -22,13 +22,41 @@ interface Props {
   self?: boolean;
   isPreview: boolean;
   seed?: number[];
+  cursorRef?: RefObject<HTMLElement | null>;
   cursorNode?: ReactNode;
   nameNode: ReactNode;
 }
 
 export const Content = memo<Props>(
-  ({ channelId, source, entities, isAction, nameNode, seed, isPreview, cursorNode = null, self = false }) => {
+  ({
+    channelId,
+    source,
+    entities,
+    isAction,
+    nameNode,
+    seed,
+    isPreview,
+    cursorRef,
+    cursorNode = null,
+    self = false,
+  }) => {
     const ref = useRef<HTMLDivElement | null>(null);
+    const toolbarHandle = useRef<CursorToolbarHandle | null>(null);
+    useEffect(() => {
+      if (!isPreview || !self) return;
+      const handle = window.setTimeout(() => {
+        toolbarHandle.current?.update();
+      }, 8);
+      return () => window.clearTimeout(handle);
+    });
+    const cursorToolbar = useMemo(() => {
+      if (!isPreview || !self || !cursorRef) return null;
+      return (
+        <Delay timeout={300}>
+          <SelfCursorToolbar cursorRef={cursorRef} ref={toolbarHandle} contentRef={ref} />
+        </Delay>
+      );
+    }, [cursorRef, isPreview, self]);
     const rng: Prando | undefined = useMemo(() => makeRng(seed), [seed]);
     const entityNodeList = useMemo(() => {
       if (entities.length === 0) {
@@ -54,23 +82,22 @@ export const Content = memo<Props>(
       });
     }, [cursorNode, entities, rng, source]);
     return (
-      <div
-        ref={ref}
-        className={clsx(
-          'relative h-full whitespace-pre-wrap break-all pr-6',
-          self ? 'pb-12' : '',
-          isPreview ? 'animate-pulse' : '',
-        )}
-      >
-        {isAction && <span className="text-surface-400 mr-1">*</span>}
-        {isAction && nameNode}
-        {entityNodeList}
-        {self && isPreview && (
-          <Delay timeout={300}>
-            <SelfCursorToolbar contentRef={ref} />
-          </Delay>
-        )}
-      </div>
+      <>
+        <div
+          ref={ref}
+          className={clsx(
+            'relative h-full whitespace-pre-wrap break-all pr-6',
+            self ? 'pb-12' : '',
+            isPreview ? 'animate-pulse' : '',
+          )}
+        >
+          {isAction && <span className="text-surface-400 mr-1">*</span>}
+          {isAction && nameNode}
+          {entityNodeList}
+        </div>
+
+        {cursorToolbar}
+      </>
     );
   },
 );

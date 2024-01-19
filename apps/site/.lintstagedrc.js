c4a7ec41293f0127ce3295aa4ac5c60af49f52cf
uonr <dev@yuru.me>
2024-01-20 03:32:51 +0900

    chore: update and remove unnecessary dependencies

--- apps/site/.lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-// https://nextjs.org/docs/basic-features/eslint#lint-staged
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};

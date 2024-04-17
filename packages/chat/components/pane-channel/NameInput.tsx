ec0ee45e488238616a6b5cabbc4adf682e2f7155
uonr <dev@yuru.me>
2024-04-17 22:31:04 +0900

    feat(chat): save the default name in the name edit popover

--- packages/chat/components/pane-channel/NameInput.tsx
+++ packages/chat/components/pane-channel/NameInput.tsx
@@ -1,25 +1,61 @@
-import { useAtom } from 'jotai';
-import { FC } from 'react';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { FC, useMemo } from 'react';
 import { TextInput } from '@boluo/ui/TextInput';
 import { useComposeAtom } from '../../hooks/useComposeAtom';
+import { selectAtom } from 'jotai/utils';
+import { Button } from '@boluo/ui/Button';
+import { FormattedMessage } from 'react-intl';
+import { useEditChannelCharacterName } from '../../hooks/useEditChannelCharacterName';
 
-interface Props {
-  id?: string;
-  className?: string;
-  setInGame?: boolean;
-  placeholder?: string;
-}
-
-export const NameInput: FC<Props> = ({ id, className, setInGame = false, placeholder }) => {
-  const composeAtom = useComposeAtom();
-  const [compose, dispatch] = useAtom(composeAtom);
+const AsDefaultButton: FC<{ channelId: string; inputedName: string; disabled: boolean }> = ({
+  channelId,
+  disabled,
+  inputedName,
+}) => {
+  const { trigger, isMutating } = useEditChannelCharacterName(channelId);
+  const handleClick = () => {
+    void trigger(
+      { characterName: inputedName },
+      {
+        rollbackOnError: true,
+      },
+    );
+  };
   return (
-    <TextInput
-      id={id}
-      value={compose.inputedName}
-      className={className}
-      placeholder={placeholder}
-      onChange={(e) => dispatch({ type: 'setInputedName', payload: { inputedName: e.target.value, setInGame } })}
-    />
+    <div className="text-right">
+      <Button data-small disabled={disabled || isMutating} onClick={handleClick}>
+        <FormattedMessage defaultMessage="As Default" />
+      </Button>
+    </div>
+  );
+};
+
+export const NameEditInput: FC<{
+  id?: string;
+  channelId: string;
+  setInGame?: boolean;
+  defaultName: string;
+}> = ({ id, setInGame = false, defaultName, channelId }) => {
+  const composeAtom = useComposeAtom();
+  const inputedNameAtom = useMemo(() => selectAtom(composeAtom, ({ inputedName }) => inputedName), [composeAtom]);
+  const inputedName = useAtomValue(inputedNameAtom);
+  const dispatch = useSetAtom(composeAtom);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'setInputedName', payload: { inputedName: e.target.value, setInGame } });
+  };
+
+  return (
+    <>
+      <div>
+        <TextInput id={id} value={inputedName} className="w-full" placeholder={defaultName} onChange={handleChange} />
+      </div>
+      <div className="text-right">
+        <AsDefaultButton
+          channelId={channelId}
+          inputedName={inputedName}
+          disabled={!inputedName || inputedName === defaultName}
+        />
+      </div>
+    </>
   );
 };

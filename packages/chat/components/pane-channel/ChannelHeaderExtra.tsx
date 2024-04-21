c8b538907d4ea0c4239d763928a027dc49572ddc
uonr <dev@yuru.me>
2024-04-22 06:19:40 +0900

    feat(chat): remove the character name edit input in the channel header

--- packages/chat/components/pane-channel/ChannelHeaderExtra.tsx
+++ packages/chat/components/pane-channel/ChannelHeaderExtra.tsx
@@ -1,9 +1,8 @@
-import { PrimitiveAtom, useAtom, useAtomValue } from 'jotai';
+import { PrimitiveAtom, useAtom } from 'jotai';
 import { FC } from 'react';
 import { ChannelHeaderState } from './ChannelHeader';
 import { ChannelHeaderFilterBar } from './ChannelHeaderFilterBar';
 import { ChannelHeaderMore } from './ChannelHeaderMore';
-import { ChannelHeaderEditCharacter } from './ChannelHeaderEditCharacter';
 
 interface Props {
   channelId: string;
@@ -13,8 +12,6 @@ interface Props {
 export const ChannelHeaderExtra: FC<Props> = ({ stateAtom, channelId }) => {
   const [headerState, setHeaderState] = useAtom(stateAtom);
   switch (headerState) {
-    case 'CHARACTER':
-      return <ChannelHeaderEditCharacter channelId={channelId} exitEdit={() => setHeaderState('DEFAULT')} />;
     case 'MORE':
       return <ChannelHeaderMore channelId={channelId} setHeaderState={setHeaderState} />;
     case 'FILTER':

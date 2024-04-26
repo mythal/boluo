024f056edca4e85df3a7992a701eaeb503d55f8a
uonr <dev@yuru.me>
2024-04-27 07:36:54 +0900

    feat: more metadata

--- apps/spa/app/(main)/[lang]/layout.tsx
+++ apps/spa/app/(main)/[lang]/layout.tsx
@@ -3,10 +3,22 @@ import { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { Providers } from './Providers';
 import '@boluo/ui/tailwind.css';
+import { getIntl } from '@boluo/common/server';
 
-export const metadata: Metadata = {
-  title: 'Boluo',
-};
+interface Params {
+  lang: Locale;
+}
+
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const intl = getIntl(params);
+  const boluo = intl.formatMessage({ defaultMessage: 'Boluo' });
+  return {
+    title: boluo,
+    description: intl.formatMessage({ defaultMessage: 'A chat application designed specifically for playing RPGs.' }),
+    applicationName: boluo,
+    manifest: `/${params.lang}.webmanifest`,
+  };
+}
 
 export default function RootLayout({
   children,

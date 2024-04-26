211b5c3d75ffe1ca331374b5aa415f7f626a6e76
uonr <dev@yuru.me>
2024-04-27 08:55:59 +0900

    feat(chat): code block color

--- packages/chat/components/entities/EntityCodeBlock.tsx
+++ packages/chat/components/entities/EntityCodeBlock.tsx
@@ -14,7 +14,7 @@ export const EntityCodeBlock: FC<Props> = ({
 }) => {
   return (
     <div className="py-2">
-      <pre className="bg-surface-200 border-surface-300 font-pixel rounded border px-2 py-1 shadow-sm">
+      <pre className="bg-code-bg border-code-border text-code-text font-pixel rounded border px-2 py-1 shadow-sm">
         {source.substring(start, start + len)}
       </pre>
     </div>

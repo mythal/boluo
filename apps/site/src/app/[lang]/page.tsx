554137b334a60468618eab3f9779db124bfb6775
uonr <dev@yuru.me>
2024-05-01 11:22:51 +0900

    feat(site): simple new homepage

--- apps/site/src/app/[lang]/page.tsx
+++ apps/site/src/app/[lang]/page.tsx
@@ -1,9 +1,60 @@
-import HomePage from './HomePage';
+import { LangParams, getIntl } from '@boluo/common/server';
+import Link from 'next/link';
+import { FC, ReactNode } from 'react';
+import { UserOperations } from '../../components/UserOperations';
 
-export default function Page() {
+const Card: FC<{ children: ReactNode; className?: string }> = ({ children, className = '' }) => {
   return (
-    <main className="p-4">
-      <HomePage />
+    <div
+      className={[
+        'bg-card-bg border-card-border shadow-1/2 shadow-card-shadow max-w-md rounded-sm border p-6',
+        className,
+      ].join(' ')}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Para: FC<{ children: ReactNode; className?: string }> = ({ children, className = '' }) => {
+  return <p className={['py-1', className].join(' ')}>{children}</p>;
+};
+
+export default function Page({ params }: { params: LangParams }) {
+  const intl = getIntl(params);
+  return (
+    <main className="mx-auto p-4 md:container md:p-8">
+      <div className="grid max-w-3xl grid-cols-1 grid-rows-[auto_auto_auto] gap-2 sm:grid-cols-2 ">
+        <Card className="row-span-3">
+          <Para className="italic">{intl.formatMessage({ defaultMessage: 'A chat application' })}</Para>
+          <h1 className="py-2 text-center text-xl">{intl.formatMessage({ defaultMessage: 'Boluo' })}</h1>
+
+          <div className="py-2">
+            <div className="bg-highest/5 flex h-20 w-full items-center justify-center italic">An Awesome Logo</div>
+          </div>
+
+          <div>Some introduction text here.</div>
+        </Card>
+
+        <Card>
+          <UserOperations />
+        </Card>
+        <Card>
+          <div className="font-bold">{intl.formatMessage({ defaultMessage: 'News' })}</div>
+          <Para>Nothing new here.</Para>
+        </Card>
+        <Card>
+          <Para>
+            {intl.formatMessage({
+              defaultMessage:
+                'This is latest version of Boluo, which is still under active development. If you perfer the legacy version, please visit',
+            })}{' '}
+            <Link className="link" href="https://old.boluo.chat">
+              old.boluo.chat
+            </Link>
+          </Para>
+        </Card>
+      </div>
     </main>
   );
 }
---
+++ apps/site/src/components/UserOperations.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { get } from '@boluo/api-browser';
+import { useQueryUser } from '@boluo/common';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useCallback } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useSWRConfig } from 'swr';
+
+const useLogout = () => {
+  const router = useRouter();
+  const { mutate } = useSWRConfig();
+
+  return useCallback(async () => {
+    await get('/users/logout', null);
+    await mutate(['/users/query'], null);
+    await mutate(['/users/get_me'], null);
+    router.refresh();
+  }, [mutate, router]);
+};
+
+export const UserOperations = () => {
+  const intl = useIntl();
+  const { data: currentUser, isLoading } = useQueryUser();
+  const logout = useLogout();
+  const appUrl = process.env.APP_URL || '';
+  if (!appUrl) {
+    alert('Please set the APP_URL environment variable.');
+    return null;
+  }
+  if (isLoading) return <div>Loading...</div>;
+  if (!currentUser)
+    return (
+      <div>
+        <FormattedMessage
+          defaultMessage="You are not logged in, {signUp} or {login}."
+          values={{
+            signUp: (
+              <Link className="link" href="/account/sign-up">
+                Sign Up
+              </Link>
+            ),
+            login: (
+              <Link className="link" href="/account/login">
+                Login
+              </Link>
+            ),
+          }}
+        />
+      </div>
+    );
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <FormattedMessage defaultMessage="Hi, {username}!" values={{ username: currentUser.nickname }} /> (
+        <a href="#" className="link" onClick={logout}>
+          <FormattedMessage defaultMessage="Logout" />
+        </a>
+        )
+      </div>
+      <div>
+        <Link className="link text-lg" href={`${appUrl}/${intl.locale}`} target="_blank">
+          Open Boluo
+        </Link>
+      </div>
+    </div>
+  );
+};

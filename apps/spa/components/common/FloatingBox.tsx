75370a4f24637b9f41e1bba9057d63c30ddbd329
uonr <dev@yuru.me>
2024-05-02 08:25:20 +0900

    feat(spa): the styles of `FloatingBox`

--- apps/spa/components/common/FloatingBox.tsx
+++ apps/spa/components/common/FloatingBox.tsx
@@ -6,7 +6,11 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 export const FloatingBox = React.forwardRef<HTMLDivElement, Props>(({ children, ...props }, ref) => {
   return (
-    <div {...props} className="bg-floating-bg border-lowest rounded border p-3 text-sm shadow-lg" ref={ref}>
+    <div
+      {...props}
+      className="bg-card-bg border-card-border shadow-1/2 shadow-card-shadow rounded-sm border p-3 text-sm"
+      ref={ref}
+    >
       {children}
     </div>
   );

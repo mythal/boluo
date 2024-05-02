8da7f4153987106232ba8a59175f7aec39373eea
uonr <dev@yuru.me>
2024-05-02 15:46:36 +0900

    Revert "fix(spa): use next/navigation to set hash"
    This reverts commit 4ab005b8fcd409ca131d614b173822142ed592e8.

--- apps/spa/components/App.tsx
+++ apps/spa/components/App.tsx
@@ -3,15 +3,12 @@
 import React, { useEffect, useState } from 'react';
 import Chat from './Chat';
 import { ChatSkeleton } from './ChatSkeleton';
-import { useSetNavigation } from '../hooks/useSetNavigation';
 
 export const App = () => {
   const [isClient, setIsClient] = useState(false);
-  useSetNavigation();
   useEffect(() => {
     setIsClient(true);
   }, []);
-
   if (!isClient) return <ChatSkeleton />;
 
   return <Chat />;

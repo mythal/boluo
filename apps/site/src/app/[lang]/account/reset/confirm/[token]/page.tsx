323954f35f798580588bd8651435cdec91b8b268
uonr <dev@yuru.me>
2024-05-04 06:34:36 +0900

    feat(site): add title of reset password pages

--- apps/site/src/app/[lang]/account/reset/confirm/[token]/page.tsx
+++ apps/site/src/app/[lang]/account/reset/confirm/[token]/page.tsx
@@ -1,4 +1,14 @@
+import { LangParams, getIntl } from '@boluo/common/server';
 import { ConfirmResetPassword } from './ConfirmResetPassword';
+import { Metadata } from 'next';
+
+export function generateMetadata({ params }: { params: LangParams }): Metadata {
+  const intl = getIntl(params);
+
+  return {
+    title: intl.formatMessage({ defaultMessage: 'Reset Password' }),
+  };
+}
 
 interface Props {
   params: { token: string };

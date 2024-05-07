531e0075f83adbe567f2e8bd4cb3774bb20c7041
uonr <dev@yuru.me>
2024-05-07 15:39:28 +0900

    feat(spa): remove sentry integrations

--- apps/spa/components/Chat.tsx
+++ apps/spa/components/Chat.tsx
@@ -43,14 +43,7 @@ if (typeof window !== 'undefined' && process.env.SENTRY_DSN) {
         environment: process.env.NODE_ENV,
         dsn: process.env.SENTRY_DSN,
         tunnel: process.env.SENTRY_TUNNEL,
-        integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-        // Performance Monitoring
-        tracesSampleRate: 1.0, //  Capture 100% of the transactions
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-        // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        integrations: [],
       });
       console.log('Sentry ready');
     })

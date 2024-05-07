a1ccd5a8a57464ce0241fa3f21bc443dc36784d8
uonr <dev@yuru.me>
2024-05-07 13:26:18 +0900

    feat(spa): crash button

--- apps/spa/components/PaneHelp.tsx
+++ apps/spa/components/PaneHelp.tsx
@@ -4,10 +4,13 @@ import { FormattedMessage } from 'react-intl';
 import { ClosePaneButton } from './ClosePaneButton';
 import { PaneBox } from './PaneBox';
 import { PaneHeaderBox } from './PaneHeaderBox';
+import { devMode } from '../state/dev.atoms';
+import { useAtomValue } from 'jotai';
 
 interface Props {}
 
 export const PaneHelp: FC<Props> = () => {
+  const dev = useAtomValue(devMode);
   return (
     <PaneBox
       header={
@@ -17,6 +20,18 @@ export const PaneHelp: FC<Props> = () => {
       }
     >
       <div className="p-pane">Coming soon</div>
+
+      {dev && (
+        <div>
+          <button
+            onClick={() => {
+              throw new Error('Crash!');
+            }}
+          >
+            Crash!
+          </button>
+        </div>
+      )}
     </PaneBox>
   );
 };

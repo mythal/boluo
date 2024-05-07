d34bb2e39276c8b10f2d6b5014ac9e9471a92d81
uonr <dev@yuru.me>
2024-05-07 10:36:34 +0900

    feat(spa): compose error boundry

---
+++ apps/spa/components/compose/ComposeErrorBoundry.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { useChannelAtoms } from '../../hooks/useChannelAtoms';
+import { useAtomValue } from 'jotai';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface State {
+  error: unknown;
+}
+
+export class ComposeErrorBoundry extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { error: null };
+  }
+  static getDerivedStateFromError(error: unknown) {
+    return { error: error };
+  }
+  override render() {
+    if (this.state.error != null) {
+      return <ShowError />;
+    }
+    return this.props.children;
+  }
+}
+
+export const ShowError: FC = () => {
+  const { composeAtom } = useChannelAtoms();
+  const { source } = useAtomValue(composeAtom);
+  return (
+    <div className="px-4 py-2">
+      <FormattedMessage defaultMessage="The input box crashed." />
+      {source && (
+        <>
+          {' '}
+          <FormattedMessage defaultMessage="Your last input was:" />
+          <pre className="py-2">{source}</pre>
+        </>
+      )}
+    </div>
+  );
+};

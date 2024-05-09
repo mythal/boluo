515a32ede60f0965a1452d61142b662fc9caf6a0
uonr <dev@yuru.me>
2024-05-09 12:45:59 +0900

    chore(server): remove migrations

--- apps/server/migrations/20240503215709_channel-type.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE channels
-    DROP COLUMN IF EXISTS "type";
--- apps/server/migrations/migrations/20240507071932_space-ext.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE spaces_extension
-(
-    "space_id"  uuid  NOT NULL PRIMARY KEY
-        CONSTRAINT "extension_space" REFERENCES spaces (id) ON DELETE CASCADE,
-    "settings" jsonb NOT NULL DEFAULT '{}'
-);

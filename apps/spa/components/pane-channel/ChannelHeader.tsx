666088f20dc3386c188e9e66b0c9bc524532a5eb
uonr <dev@yuru.me>
2024-05-15 20:22:27 +0900

    feat(spa): change icon of the channel pane

--- apps/spa/components/pane-channel/ChannelHeader.tsx
+++ apps/spa/components/pane-channel/ChannelHeader.tsx
@@ -1,5 +1,5 @@
 import { Channel } from '@boluo/api';
-import { Hash, LockedHash } from '@boluo/icons';
+import { Drama, Hash } from '@boluo/icons';
 import { atom } from 'jotai';
 import { FC, useMemo } from 'react';
 import { useChannelId } from '../../hooks/useChannelId';
@@ -7,20 +7,43 @@ import { PaneHeaderBox } from '../PaneHeaderBox';
 import { ChannelHeaderExtra } from './ChannelHeaderExtra';
 import { ChannelHeaderOperations } from './ChannelHeaderOperations';
 import { useChannel } from '../../hooks/useChannel';
+import { FormattedMessage } from 'react-intl';
 
 export type ChannelHeaderState = 'DEFAULT' | 'MORE' | 'FILTER' | 'CHARACTER';
 
 const ChannelName: FC<{ channel: Channel | null | undefined }> = ({ channel }) => {
-  return <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">{channel?.name ?? '...'}</span>;
+  if (!channel) {
+    return <span>...</span>;
+  }
+  return (
+    <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+      {!channel.isPublic ? (
+        <span className="text-text-lighter mr-1">
+          [<FormattedMessage defaultMessage="Secret" />]
+        </span>
+      ) : (
+        ''
+      )}
+      {channel.name}
+    </span>
+  );
 };
 
 export const ChannelHeader: FC = () => {
   const channelId = useChannelId();
   const channel = useChannel();
   const headerStateAtom = useMemo(() => atom<ChannelHeaderState>('DEFAULT'), []);
+  const icon = useMemo(() => {
+    if (!channel) {
+      return <Hash />;
+    } else if (channel.type === 'IN_GAME') {
+      return <Drama />;
+    }
+    return <Hash />;
+  }, [channel]);
   return (
     <PaneHeaderBox
-      icon={channel && !channel.isPublic ? <LockedHash /> : <Hash />}
+      icon={icon}
       operators={channel ? <ChannelHeaderOperations stateAtom={headerStateAtom} channel={channel} /> : null}
       extra={<ChannelHeaderExtra channelId={channelId} stateAtom={headerStateAtom} />}
     >

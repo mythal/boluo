e8d95ced9362041c5fcdda4e48b8e1773b60e4b6
uonr <dev@yuru.me>
2024-05-16 17:51:41 +0900

    feat(spa): use pages router
    Currently there is an issue where hash is not maintained under devserver.
    
    Anyway, the SPA is relatively simple, and it's not difficult to switch again later if needed.

---
+++ apps/spa/pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import '@boluo/ui/tailwind.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const Redirector = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const locale = navigator.language.toLocaleLowerCase();
+    const hash = location.hash;
+    let subpath = '/en';
+    if (locale.startsWith('ja')) {
+      subpath = '/ja';
+    } else if (locale.startsWith('zh')) {
+      subpath = '/zh-CN';
+    }
+
+    void router.replace(`${subpath}${hash}`);
+  }, [router]);
+  return <div className="animate-pulse p-8">Redirecting...</div>;
+};
+
+export default function Page(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Boluo Chat</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="description" content="A chat application designed specifically for playing RPGs." />
+      </Head>
+      <Redirector />
+    </>
+  );
+}

10be924183f55f3bd16d2937f617433b07544e1c
uonr <dev@yuru.me>
2024-05-17 14:35:47 +0900

    refactor(spa): merge the message box into the `ChatItemMessage`

--- apps/spa/components/pane-channel/ChatItemMessage.tsx
+++ apps/spa/components/pane-channel/ChatItemMessage.tsx
@@ -2,12 +2,10 @@
 import clsx from 'clsx';
 import { FC, useEffect, useMemo, useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { fromRawEntities } from '../../interpreter/entities';
-import { emptyParseResult, ParseResult } from '../../interpreter/parse-result';
+import { ParseResult } from '../../interpreter/parse-result';
 import { MessageItem } from '../../state/channel.types';
 import { ChatItemMessageShowWhisper } from './ChatItemMessageShowWhisper';
 import { Content } from './Content';
-import { MessageBox } from './MessageBox';
 import { MessageMedia } from './MessageMedia';
 import { Name } from './Name';
 import { useQueryUser } from '@boluo/common';
@@ -15,8 +13,16 @@ import { ResolvedTheme } from '@boluo/theme';
 import { messageToParsed } from '../../interpreter/to-parsed';
 import { useIsScrolling } from '../../hooks/useIsScrolling';
 import { useReadObserve } from '../../hooks/useReadObserve';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { Message } from '@boluo/api';
+import { ReactNode } from 'react';
+import { Delay } from '../Delay';
+import { MessageReorderHandle } from './MessageReorderHandle';
+import { MessageToolbox } from './MessageToolbox';
+import { MessageTime } from './MessageTime';
 
-interface Props {
+export const ChatItemMessage: FC<{
   iAmAdmin: boolean;
   iAmMaster: boolean;
   message: MessageItem;
@@ -26,18 +32,7 @@ interface Props {
   continuous?: boolean;
   overlay?: boolean;
   theme: ResolvedTheme;
-}
-
-export const ChatItemMessage: FC<Props> = ({
-  message,
-  self,
-  continuous = false,
-  iAmAdmin,
-  iAmMaster,
-  overlay = false,
-  theme,
-  isLast,
-}) => {
+}> = ({ message, self, continuous = false, iAmAdmin, iAmMaster, overlay = false, theme, isLast }) => {
   const isScrolling = useIsScrolling();
   const { isMaster, isAction, optimistic } = message;
   const { data: user } = useQueryUser(message.senderId);
@@ -112,5 +107,101 @@ export const ChatItemMessage: FC<Props> = ({
         {message.mediaId != null && <MessageMedia className="pt-2" media={message.mediaId} />}
       </div>
     </MessageBox>
+  );
+};
+
+const MessageBox: FC<{
+  className?: string;
+  children: ReactNode;
+  message: Message;
+  draggable?: boolean;
+  mini?: boolean;
+  overlay?: boolean;
+  optimistic?: boolean;
+  self: boolean;
+  iAmMaster: boolean;
+  iAmAdmin: boolean;
+  isScrolling: boolean;
+  inGame: boolean;
+}> = ({
+  className = '',
+  inGame,
+  children,
+  draggable = false,
+  overlay = false,
+  message,
+  mini = false,
+  isScrolling,
+  optimistic = false,
+  self,
+  iAmMaster,
+  iAmAdmin,
+}) => {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging, setActivatorNodeRef } = useSortable({
+    id: message.id,
+    data: { message },
+    disabled: !draggable || isScrolling,
+  });
+
+  const style = useMemo(
+    () => ({
+      transform: CSS.Transform.toString(transform),
+      transition,
+    }),
+    [transform, transition],
+  );
+  const handle = useMemo(
+    () =>
+      draggable ? (
+        <MessageReorderHandle
+          ref={setActivatorNodeRef}
+          attributes={attributes}
+          listeners={listeners}
+          loading={optimistic}
+        >
+          <MessageTime message={message} />
+        </MessageReorderHandle>
+      ) : (
+        <div className="text-message-time-text col-span-1 row-span-full h-full text-right">
+          <MessageTime message={message} />
+        </div>
+      ),
+    [attributes, draggable, listeners, message, optimistic, setActivatorNodeRef],
+  );
+  const toolbox = useMemo(
+    () => (
+      <Delay timeout={400}>
+        <div
+          className={clsx(
+            'absolute right-3 top-0 z-10 hidden max-h-full -translate-y-4 group-hover:z-20 group-hover:block',
+          )}
+        >
+          <MessageToolbox message={message} self={self} iAmAdmin={iAmAdmin} iAmMaster={iAmMaster} />
+        </div>
+      </Delay>
+    ),
+    [iAmAdmin, iAmMaster, message, self],
+  );
+  return (
+    <div
+      data-overlay={overlay}
+      data-in-game={inGame}
+      className={clsx(
+        'group relative grid grid-flow-col items-center gap-2 py-2 pl-2 pr-2',
+        'grid-cols-[4rem_minmax(0,1fr)]',
+        '@2xl:grid-cols-[4rem_12rem_minmax(0,1fr)]',
+        !mini && '@2xl:grid-rows-1 grid-rows-[auto_auto]',
+        inGame ? 'bg-message-inGame-bg hover:bg-message-inGame-hover-bg' : 'hover:bg-message-hover-bg',
+        'data-[overlay=true]:bg-surface-300/30 data-[overlay=true]:data-[in-game=true]:bg-message-inGame-bg/75 data-[overlay=true]:backdrop-blur-sm',
+        isDragging && 'opacity-0',
+        className,
+      )}
+      ref={setNodeRef}
+      style={style}
+    >
+      {handle}
+      {children}
+      {(self || iAmMaster || iAmAdmin) && toolbox}
+    </div>
   );
 };

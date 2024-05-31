4155b6667cdfeda9e74782c25bb60c338518cdc8
uonr <dev@yuru.me>
2024-05-31 22:23:33 +0900

    feat(spa): improve the style of `PaneBanner`

--- apps/spa/components/PaneBanner.tsx
+++ apps/spa/components/PaneBanner.tsx
@@ -1,30 +1,41 @@
 import clsx from 'clsx';
 import { X } from '@boluo/icons';
 import { type FC, useCallback } from 'react';
-import { Button } from '@boluo/ui/Button';
-import Icon from '@boluo/ui/Icon';
 import { type Banner, useSetBanner } from '../hooks/useBanner';
+import { useIntl } from 'react-intl';
 
 interface Props {
   banner: Banner;
 }
 export const PaneBanner: FC<Props> = ({ banner }) => {
-  const setBanner = useSetBanner();
-  const closeBanner = useCallback(() => setBanner(null), [setBanner]);
   const level = banner.level ?? 'INFO';
   return (
     <div
       className={clsx(
-        'flex items-center justify-between p-4 text-sm',
-        level === 'INFO' && 'bg-surface-100',
-        level === 'WARNING' && 'bg-warning-50',
-        level === 'ERROR' && 'bg-error-50',
+        'pl-pane group/banner flex items-center justify-between py-2 pr-[6px] text-sm',
+        level === 'INFO' && 'bg-banner-info-bg',
+        level === 'WARNING' && 'bg-banner-warning-bg',
+        level === 'ERROR' && 'bg-banner-error-bg',
       )}
     >
       {banner.content}
-      <Button data-small onClick={closeBanner}>
-        <Icon icon={X} />
-      </Button>
+      <CloseBannerButton />
     </div>
+  );
+};
+
+export const CloseBannerButton: FC = () => {
+  const setBanner = useSetBanner();
+  const closeBanner = useCallback(() => setBanner(null), [setBanner]);
+  const intl = useIntl();
+  const label = intl.formatMessage({ defaultMessage: 'Close' });
+  return (
+    <button
+      aria-label={label}
+      onClick={closeBanner}
+      className="bg-banner-close-bg/50 group-hover/banner:bg-banner-close-bg inline-flex rounded-sm p-1.5 text-sm"
+    >
+      <X />
+    </button>
   );
 };

23c1fffecc37a054d0b7260a360ae57c1ff6b5de
uonr <dev@yuru.me>
2024-06-01 00:14:34 +0900

    feat(spa): add the `GreetingHeader`

---
+++ apps/spa/components/GreetingHeader.tsx
@@ -0,0 +1,24 @@
+import { type FC } from 'react';
+import { ThemeSelect } from './pane-settings/ThemeSelect';
+import { FormattedMessage } from 'react-intl';
+import { LocaleSelect } from './pane-settings/LocaleSelect';
+
+export const GreetingHeader: FC = () => {
+  return (
+    <div className="flex justify-end gap-4 text-sm">
+      <label className="flex items-baseline gap-2">
+        <span className="flex-none">
+          <FormattedMessage defaultMessage="Theme" />
+        </span>
+        <ThemeSelect />
+      </label>
+
+      <label className="flex items-baseline gap-2">
+        <span className="flex-none">
+          <FormattedMessage defaultMessage="Language" />
+        </span>
+        <LocaleSelect />
+      </label>
+    </div>
+  );
+};
--- apps/spa/components/PaneSpaceGreeting.tsx
+++ apps/spa/components/PaneSpaceGreeting.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react';
 import { PaneBox } from './PaneBox';
 import { useQuerySpace } from '../hooks/useQuerySpace';
+import { GreetingHeader } from './GreetingHeader';
 
 interface Props {
   spaceId: string;
@@ -10,7 +11,10 @@ export const PaneSpaceGreeting: FC<Props> = ({ spaceId }) => {
   const { data: space } = useQuerySpace(spaceId);
   return (
     <PaneBox header={null} grow>
-      <div className="p-pane text-text-lighter flex h-full items-center justify-center">{space?.name || '...'}</div>
+      <div className="p-pane h-full">
+        <GreetingHeader />
+        <div className="text-text-lighter flex h-full items-center justify-center ">{space?.name || '...'}</div>
+      </div>
     </PaneBox>
   );
 };

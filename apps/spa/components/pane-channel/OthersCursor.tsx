614254a78003ea9b0e908d4f2ef03bfb85720027
uonr <dev@yuru.me>
2024-06-01 21:51:00 +0900

    refactor(spa): remove `OthersCursor`

--- apps/spa/components/pane-channel/OthersCursor.tsx
+++
@@ -1,7 +0,0 @@
-import { type FC } from 'react';
-
-interface Props {}
-
-export const OthersCursor: FC<Props> = () => (
-  <span className="bg-highest absolute inline-block h-6 w-2 opacity-40"></span>
-);

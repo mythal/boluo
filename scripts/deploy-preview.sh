442ea3ce4a452d1ffb3928f6dab606f6820cea16
uonr <dev@yuru.me>
2024-08-20 05:31:45 +0900

    docs: add some comments to `deploy-preview.sh`

--- scripts/deploy-preview.sh
+++ scripts/deploy-preview.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+# Deploy the current commit to Cloudflare Pages preview environment
+
+# https://app-preview.boluo.chat
+# https://old-preview.boluo.chat
+# https://site-preview.boluo.chat
 
 git push -f origin "$(git symbolic-ref --short HEAD)":preview

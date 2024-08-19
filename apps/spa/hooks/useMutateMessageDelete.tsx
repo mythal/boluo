858966e1aacd26417c2d04479ac9aede1a53f0f3
uonr <dev@yuru.me>
2024-08-20 01:16:17 +0900

    fix: already deleted

--- apps/spa/hooks/useMutateMessageDelete.tsx
+++ apps/spa/hooks/useMutateMessageDelete.tsx
@@ -1,16 +1,20 @@
 import useSWRMutation, { type MutationFetcher, type SWRMutationConfiguration } from 'swr/mutation';
 import { post } from '@boluo/api-browser';
-import { type ApiError, type Message } from '@boluo/api';
+import { type ApiError } from '@boluo/api';
 
 const path = '/messages/query';
 type Key = readonly [typeof path, string];
 
-const updater: MutationFetcher<Message, Key> = async ([_, messageId]) => {
+const updater: MutationFetcher<string, Key> = async ([_, messageId]) => {
   const result = await post('/messages/delete', { id: messageId }, {});
-  return result.unwrap();
+  if (result.isErr && result.err.code === 'NOT_FOUND') {
+    return messageId;
+  }
+  result.unwrap();
+  return messageId;
 };
 
 export const useMutateMessageDelete = (
   messageId: string,
-  options: SWRMutationConfiguration<Message, ApiError, Key, never> = { revalidate: false },
-) => useSWRMutation<Message, ApiError, Key>([path, messageId] as const, updater, options);
+  options: SWRMutationConfiguration<string, ApiError, Key, never> = { revalidate: false },
+) => useSWRMutation<string, ApiError, Key>([path, messageId] as const, updater, options);

4f3de59909688feb34be069d3704756e10949f51
uonr <dev@yuru.me>
2024-08-25 22:46:09 +0900

    fix(server): ensure the max pos is largest

--- apps/server/src/pos.rs
+++ apps/server/src/pos.rs
@@ -10,6 +10,19 @@ fn create_pos_key(channel_id: Uuid, message_id: Uuid) -> String {
     format!("channel:{channel_id}:preview:{message_id}:pos")
 }
 
+pub async fn ensure_pos_largest(
+    cache: &mut crate::cache::Connection,
+    channel_id: Uuid,
+    pos: i32,
+) -> Result<(), CacheError> {
+    let max_pos_key = create_max_pos_key(&channel_id);
+    let current_max: i32 = cache.inner.get::<_, Option<i32>>(&max_pos_key).await?.unwrap_or(1);
+    if pos > current_max {
+        cache.inner.set::<_, _, ()>(&max_pos_key, pos).await?;
+    }
+    Ok(())
+}
+
 pub async fn alloc_new_pos(
     db: &mut sqlx::PgConnection,
     cache: &mut crate::cache::Connection,

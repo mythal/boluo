7a8069689e73a995a66b28e2a7c045ca19b5be35
uonr <dev@yuru.me>
2024-09-08 03:33:35 +0900

    fix(spa): workaround

--- apps/spa/components/ClientProviders.tsx
+++ apps/spa/components/ClientProviders.tsx
@@ -2,7 +2,7 @@
 import { Provider as JotaiProvider } from 'jotai';
 import React, { type ReactNode, useCallback } from 'react';
 import { store } from '@boluo/store';
-import { SWRConfig } from 'swr';
+import { SWRConfig, type SWRConfiguration } from 'swr';
 import type { IntlMessages, Locale } from '@boluo/common/locale';
 import { IntlProvider, type ResolvedIntlConfig, ReactIntlErrorCode } from 'react-intl';
 import { ChangeLocaleContext } from '@boluo/common/hooks/useLocale';
@@ -30,6 +30,14 @@ const onError = (error: unknown, key: unknown) => {
   captureException(error, { extra: { key } });
 };
 
+const swrConfig: SWRConfiguration = {
+  refreshInterval: 60000,
+  onError,
+  onSuccess: () => {
+    // Try to workaround `R().onSuccess is not a function`
+  },
+};
+
 export function ClientProviders({ children, lang, messages }: Props) {
   const changeLocale = useCallback((locale: Locale) => {
     location.href = `/${locale}${location.hash}`;
@@ -43,12 +51,7 @@ export function ClientProviders({ children, lang, messages }: Props) {
 
   return (
     <JotaiProvider store={store}>
-      <SWRConfig
-        value={{
-          refreshInterval: 60000,
-          onError,
-        }}
-      >
+      <SWRConfig value={swrConfig}>
         <ChangeLocaleContext.Provider value={changeLocale}>
           <IntlProvider locale={lang} messages={messages} onError={handleIntlError}>
             {children}

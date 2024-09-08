4cdefa236b3dfbde58d037460ed1a531e173897a
uonr <dev@yuru.me>
2024-09-08 11:19:03 +0900

    feat(spa): more indicator

--- apps/spa/components/pane-space/SpaceJoinButton.tsx
+++ apps/spa/components/pane-space/SpaceJoinButton.tsx
@@ -1,12 +1,13 @@
 import { type ApiError, type SpaceWithMember } from '@boluo/api';
 import { post } from '@boluo/api-browser';
-import { UserPlus } from '@boluo/icons';
-import { type FC } from 'react';
+import { AlertTriangle, UserPlus } from '@boluo/icons';
+import { type ReactNode, type FC } from 'react';
 import { FormattedMessage } from 'react-intl';
 import useSWRMutation from 'swr/mutation';
 import { unwrap } from '@boluo/utils';
 import { SidebarHeaderButton } from '../sidebar/SidebarHeaderButton';
 import { useSWRConfig } from 'swr';
+import { Spinner } from '@boluo/ui/Spinner';
 
 interface Props {
   spaceId: string;
@@ -15,7 +16,11 @@ interface Props {
 export const SpaceJoinButton: FC<Props> = ({ spaceId }) => {
   const { mutate } = useSWRConfig();
   const key = ['/spaces/my_space_member', spaceId] as const;
-  const { trigger: join, isMutating: isJoining } = useSWRMutation<SpaceWithMember, ApiError, typeof key>(
+  const {
+    trigger: join,
+    isMutating: isJoining,
+    error,
+  } = useSWRMutation<SpaceWithMember, ApiError, typeof key>(
     key,
     ([_, spaceId]) => post('/spaces/join', { spaceId }, {}).then(unwrap),
     {
@@ -24,8 +29,14 @@ export const SpaceJoinButton: FC<Props> = ({ spaceId }) => {
       },
     },
   );
+  let icon: ReactNode = <UserPlus />;
+  if (isJoining) {
+    icon = <Spinner />;
+  } else if (error) {
+    icon = <AlertTriangle />;
+  }
   return (
-    <SidebarHeaderButton isLoading={isJoining} icon={<UserPlus />} onClick={() => join()}>
+    <SidebarHeaderButton className="SpaceJoinButton" isLoading={isJoining} icon={icon} onClick={() => join()}>
       <span className="text-xs">
         <FormattedMessage defaultMessage="Join Space" />
       </span>

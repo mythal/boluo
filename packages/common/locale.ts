dea76b660e9364cc1c370e6763991766c824de56
uonr <dev@yuru.me>
2024-09-13 17:34:07 +0900

    refactor(site): convert locale to lower case

--- packages/common/locale.ts
+++ packages/common/locale.ts
@@ -18,7 +18,8 @@ export const loadMessages = async (locale: Locale): Promise<IntlMessages> => {
 };
 
 export const narrowLocale = (locale: string): Locale | null => {
-  if (locale === 'zh-tw') {
+  locale = locale.toLowerCase();
+  if (locale === 'zh-tw' || locale === "zh-hant") {
     return 'zh-TW';
   } else if (locale === 'zh' || locale.startsWith('zh-')) {
     return 'zh-CN';

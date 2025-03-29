046e338fa553787927742399439046ba5a8242f1
uonr <dev@yuru.me>
2025-03-29 19:48:03 +0900

    chore: build with `pkgs.buildNpmPackage`

--- support/legacy.nix
+++ support/legacy.nix
@@ -1,55 +1,22 @@
 {
-  nodeDeps,
   pkgs,
   version,
+  pruneSource,
+  mkNpmDeps,
   ...
 }:
 let
-  inherit (pkgs) lib stdenv;
-  pruned-legacy = stdenv.mkDerivation {
-    name = "boluo-pruned-legacy-source";
-    src = lib.cleanSource ../.;
-    __contentAddressed = true;
-
-    outputHashMode = "recursive";
-    outputHashAlgo = "sha256";
-
-    installPhase = ''
-      ${pkgs.turbo}/bin/turbo prune legacy
-      mkdir -p $out
-      cp -r out/* $out
-    '';
-  };
+  src = pruneSource "legacy";
+  npmDeps = mkNpmDeps src;
 in
-pkgs.stdenv.mkDerivation {
-  src = pruned-legacy;
-  inherit version;
+pkgs.buildNpmPackage {
   pname = "boluo-legacy";
-  doCheck = true;
-  buildInputs = [
-    pkgs.nodejs
-    pkgs.cacert
-  ];
 
-  configurePhase = ''
-    runHook preConfigure
-    export HOME=$(mktemp -d)
-    runHook postConfigure
-  '';
-  buildPhase = ''
-    runHook preBuild
-    cp -r ${nodeDeps}/_napalm-install/node_modules .
-    chmod -R u+w .
-    npm run build:legacy
-    runHook postBuild
-  '';
-  checkPhase = ''
-    runHook preCheck
-    npm run check-types --filter=legacy
-    runHook postCheck
-  '';
+  inherit src version npmDeps;
+
   installPhase = ''
     mkdir $out
     cp -r apps/legacy/dist/* $out
   '';
+
 }

647028a18aea5d2ff614e3b16d2677373cd01e6e
uonr <dev@yuru.me>
2025-04-08 01:22:33 +0900

    feat(server): btree pos

--- apps/server/sql/messages/create.sql
+++ apps/server/sql/messages/create.sql
@@ -1,5 +1,5 @@
-INSERT INTO messages (sender_id, channel_id, name, text, entities, in_game, is_action, is_master, whisper_to_users, media_id, pos_p, pos_q, color)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+INSERT INTO messages (id, sender_id, channel_id, name, text, entities, in_game, is_action, is_master, whisper_to_users, media_id, pos_p, pos_q, color)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
 RETURNING
     messages AS "message!: Message";
 

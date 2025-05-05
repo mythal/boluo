8236350084e5a36cca4dc2ac2a02b6cfa211ed40
uonr <dev@yuru.me>
2025-05-06 01:25:16 +0900

    feat(storybook): basic

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -13,9 +13,9 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: { name: getAbsolutePath('@storybook/react-vite'), options: {} },
+  core: { disableTelemetry: true },
 };
 export default config;
--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import '@boluo/ui/tailwind.css';
 
 const preview: Preview = {
   parameters: { controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } } },

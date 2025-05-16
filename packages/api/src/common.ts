2177241413f8bd179f6c8b80c2e4d081754a22b2
uonr <dev@yuru.me>
2025-05-16 19:24:18 +0900

    fix(spa): appFetch

--- packages/api/src/common.ts
+++ packages/api/src/common.ts
@@ -5,33 +5,41 @@ import { ApiError } from './errors.js';
 import { isAppResponse } from './request';
 
 export async function appFetch<T>(url: string, params: RequestInit): Promise<Result<T, ApiError>> {
-  let result: Result<T, ApiError>;
+  let response: Response;
   try {
-    const response = await fetch(url, params);
-    let data: unknown;
-    try {
-      data = await response.json();
-    } catch (cause) {
-      const error: NotJsonError = { code: 'NOT_JSON', cause };
-      result = new Err(error);
-    }
-    if (isAppResponse(data)) {
-      if (data.isOk) {
-        result = new Ok(data.ok as T);
-      } else {
-        result = new Err(data.err);
-      }
-    } else {
-      const error: UnexpectedError = {
-        code: 'UNEXPECTED',
-        message: 'Got incorrect data from the server',
-        context: data,
-      };
-      result = new Err(error);
-    }
+    response = await fetch(url, params);
   } catch (cause) {
     const error: FetchFailError = { code: 'FETCH_FAIL', cause };
-    result = new Err(error);
+    return new Err(error);
   }
-  return result;
+  if (response.headers.get('content-type') !== 'application/json') {
+    const error: NotJsonError = {
+      code: 'NOT_JSON',
+      cause: new Error('Response is not JSON'),
+      body: await response.text(),
+    };
+    return new Err(error);
+  }
+
+  let data: unknown;
+  try {
+    data = await response.json();
+  } catch (cause) {
+    const error: NotJsonError = { code: 'NOT_JSON', cause };
+    return new Err(error);
+  }
+  if (isAppResponse(data)) {
+    if (data.isOk) {
+      return new Ok(data.ok as T);
+    } else {
+      return new Err(data.err);
+    }
+  } else {
+    const error: UnexpectedError = {
+      code: 'UNEXPECTED',
+      message: 'Got incorrect data from the server',
+      context: data || 'Empty data',
+    };
+    return new Err(error);
+  }
 }

c184b0abae2ce1b746661ebb1234bfa62e3235a5
uonr <dev@yuru.me>
2025-07-23 02:51:32 +0900

    feat(server): adjust config of database pool

--- apps/server/src/db.rs
+++ apps/server/src/db.rs
@@ -6,14 +6,14 @@ pub fn get_postgres_url() -> String {
     std::env::var("DATABASE_URL").expect("Failed to load Postgres connect URL")
 }
 
-#[tracing::instrument]
 pub async fn get() -> sqlx::Pool<sqlx::Postgres> {
     static POOL: OnceLock<sqlx::Pool<sqlx::Postgres>> = OnceLock::new();
     const LIFETIME: std::time::Duration = std::time::Duration::from_secs(60 * 60);
     const IDLE_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(60 * 5);
+    const ACQUIRE_SLOW_THRESHOLD: std::time::Duration = std::time::Duration::from_millis(800);
+    const ACQUIRE_TIMEOUT: std::time::Duration = std::time::Duration::from_millis(2000);
     if let Some(pool) = POOL.get() {
-        let pool = pool.clone();
-        pool
+        pool.clone()
     } else {
         let pool = sqlx::postgres::PgPoolOptions::new()
             .after_connect(|conn, _meta| {
@@ -27,16 +27,17 @@ pub async fn get() -> sqlx::Pool<sqlx::Postgres> {
                     Ok(())
                 })
             })
-            .after_release(|_conn, _meta| Box::pin(async move { Ok(true) }))
-            .max_connections(64)
+            .max_connections(32)
+            .min_connections(4)
+            .acquire_slow_threshold(ACQUIRE_SLOW_THRESHOLD)
+            .acquire_timeout(ACQUIRE_TIMEOUT)
             .max_lifetime(Some(LIFETIME))
             .idle_timeout(Some(IDLE_TIMEOUT))
             .connect(&get_postgres_url())
             .await
             .expect("Cannot connect to database");
 
-        let pool = POOL.get_or_init(move || pool).clone();
-        pool
+        POOL.get_or_init(move || pool).clone()
     }
 }
 

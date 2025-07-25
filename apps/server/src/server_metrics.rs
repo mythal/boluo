b6283f0f65f66190b0aca0e7d83711e1345ec477
uonr <dev@yuru.me>
2025-07-26 04:48:50 +0900

    feat(server): adjust `update_network_metrics`

--- apps/server/src/server_metrics.rs
+++ apps/server/src/server_metrics.rs
@@ -30,15 +30,15 @@ pub fn update_db_pool_metrics(pool: &sqlx::Pool<sqlx::Postgres>) {
 pub fn start_update_metrics() {
     tokio::task::spawn(async {
         let mut interval_4s = tokio::time::interval(std::time::Duration::from_secs(4));
-        let mut interval_16s = tokio::time::interval(std::time::Duration::from_secs(16));
+        let mut interval_8s = tokio::time::interval(std::time::Duration::from_secs(8));
         loop {
             tokio::select! {
                 _ = interval_4s.tick() => {
                     update_file_descriptor_metrics().await;
                     update_db_pool_metrics(&crate::db::get().await);
                 }
-                _ = interval_16s.tick() => {
-                    if let Err(e) = update_network_metrics() {
+                _ = interval_8s.tick() => {
+                    if let Ok(Err(e)) = tokio::task::spawn_blocking(update_network_metrics).await {
                         tracing::error!("Failed to update network metrics: {}", e);
                     }
                 }

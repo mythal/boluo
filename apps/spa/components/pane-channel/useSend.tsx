61a27b907f97882351698d01f1d93fbf9a86b3f7
uonr <dev@yuru.me>
2025-08-10 02:12:31 +0900

    feat(spa): improve error handling when sending message

--- apps/spa/components/pane-channel/useSend.tsx
+++ apps/spa/components/pane-channel/useSend.tsx
@@ -1,7 +1,7 @@
 import { type NewMessage, type EditMessage, type MemberWithUser } from '@boluo/api';
 import { patch, post } from '@boluo/api-browser';
 import { useStore } from 'jotai';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 import { useChannelAtoms } from '../../hooks/useChannelAtoms';
 import { useChannelId } from '../../hooks/useChannelId';
 import { useQueryChannelMembers } from '../../hooks/useQueryChannelMembers';
@@ -15,6 +15,7 @@ import { chatAtom } from '../../state/chat.atoms';
 import { timeout } from '@boluo/utils';
 import { type FailTo } from '../../state/channel.types';
 import { useIntl } from 'react-intl';
+import { useSetBanner } from '../../hooks/useBanner';
 
 const SEND_TIMEOUT = 8000;
 
@@ -35,29 +36,34 @@ export const useSend = () => {
     if (queryChannelMembers == null) return new Map();
     return new Map(queryChannelMembers.members.map((member) => [member.user.username, member]));
   }, [queryChannelMembers]);
+  const channelMembersMapRef = useRef(channelMembersMap);
+  channelMembersMapRef.current = channelMembersMap;
+
+  const setBanner = useSetBanner();
 
   const send = useCallback(async () => {
     const sendStartTime = Date.now();
     if (myMember == null) {
       recordWarn('Can not find current user in channel');
-      alert(
-        intl.formatMessage({
+      setBanner({
+        level: 'ERROR',
+        content: intl.formatMessage({
           defaultMessage: 'Can not send message, please check if you are in the channel.',
         }),
-      );
+      });
       return;
     }
     const nickname = myMember.user.nickname;
-    const compose = store.get(composeAtom);
+    const composeState = store.get(composeAtom);
     const parsed = store.get(parsedAtom);
     if (store.get(checkComposeAtom) != null) return;
     const composeDispatch = (action: ComposeActionUnion) => store.set(composeAtom, action);
     const chatDispatch = (action: ChatActionUnion) => store.set(chatAtom, action);
-    composeDispatch({ type: 'sent', payload: { edit: compose.edit != null } });
-    const { text, entities, whisperToUsernames } = parse(compose.source, true, {
+    composeDispatch({ type: 'sent', payload: { edit: composeState.edit != null } });
+    const { text, entities, whisperToUsernames } = parse(composeState.source, true, {
       defaultDiceFace: defaultDiceFaceRef.current,
       resolveUsername: (username) => {
-        const member = channelMembersMap.get(username);
+        const member = channelMembersMapRef.current.get(username);
         if (member == null) return null;
         return member.user.nickname;
       },
@@ -65,7 +71,7 @@ export const useSend = () => {
     let name = nickname;
     const inGame = parsed.inGame ?? defaultInGame;
     if (inGame) {
-      const inputedName = compose.inputedName.trim();
+      const inputedName = composeState.inputedName.trim();
       if (inputedName === '') {
         name = myMember.channel.characterName;
       } else {
@@ -75,13 +81,13 @@ export const useSend = () => {
     let payload:
       | { type: 'NEW'; newMessage: NewMessage }
       | { type: 'EDIT'; editMessage: EditMessage };
-    if (compose.edit == null) {
+    if (composeState.edit == null) {
       const usernameListToUserIdList = (usernames: string[]): string[] => {
-        if (channelMembersMap.size === 0 || usernames.length === 0) {
+        if (channelMembersMapRef.current.size === 0 || usernames.length === 0) {
           return [];
         }
         return usernames.flatMap((username) => {
-          const member = channelMembersMap.get(username);
+          const member = channelMembersMapRef.current.get(username);
           if (member == null) return [];
           return [member.user.id];
         });
@@ -90,7 +96,7 @@ export const useSend = () => {
         type: 'NEW',
         newMessage: {
           messageId: null,
-          previewId: compose.previewId,
+          previewId: composeState.previewId,
           channelId,
           name,
           text,
@@ -107,7 +113,8 @@ export const useSend = () => {
         payload: {
           newMessage: payload.newMessage,
           sendTime: sendStartTime,
-          media: compose.media instanceof File ? compose.media : null,
+          media: composeState.media instanceof File ? composeState.media : null,
+          composeState,
         },
       });
     } else {
@@ -115,13 +122,13 @@ export const useSend = () => {
         type: 'EDIT',
         editMessage: {
           // In edit mode, the `compose.previewId` is the message id.
-          messageId: compose.previewId,
+          messageId: composeState.previewId,
           name,
           text,
           entities,
           inGame,
           isAction: parsed.isAction,
-          mediaId: typeof compose.media === 'string' ? compose.media : null,
+          mediaId: typeof composeState.media === 'string' ? composeState.media : null,
           color: '',
         },
       };
@@ -130,35 +137,43 @@ export const useSend = () => {
         payload: {
           editMessage: payload.editMessage,
           sendTime: sendStartTime,
-          media: compose.media instanceof File ? compose.media : null,
+          media: composeState.media instanceof File ? composeState.media : null,
+          composeState,
         },
       });
     }
 
     let uploadResult: Awaited<ReturnType<typeof upload>> | null = null;
-    if (compose.media instanceof File) {
-      uploadResult = await upload(compose.media);
+    if (composeState.media instanceof File) {
+      uploadResult = await upload(composeState.media);
     }
     if (uploadResult?.isOk === false) {
       let key: string;
       let failTo: FailTo;
       if (payload.type === 'NEW') {
-        key = compose.previewId;
-        failTo = { type: 'SEND', onUpload: true, composeState: compose };
+        key = composeState.previewId;
+        failTo = { type: 'SEND', onUpload: uploadResult.err };
       } else {
         key = payload.editMessage.messageId;
-        failTo = { type: 'EDIT', onUpload: true, composeState: compose };
+        failTo = { type: 'EDIT', onUpload: uploadResult.err };
       }
       chatDispatch({ type: 'fail', payload: { failTo, key } });
       return;
     }
     const mediaId = uploadResult?.isOk ? uploadResult.some.mediaId : null;
     if (payload.type === 'EDIT') {
-      if (mediaId) payload.editMessage.mediaId = mediaId;
-      chatDispatch({
-        type: 'messageEditing',
-        payload: { editMessage: payload.editMessage, sendTime: sendStartTime, media: null },
-      });
+      if (mediaId) {
+        payload.editMessage.mediaId = mediaId;
+        chatDispatch({
+          type: 'messageEditing',
+          payload: {
+            editMessage: payload.editMessage,
+            sendTime: sendStartTime,
+            media: null,
+            composeState,
+          },
+        });
+      }
       const result = await Promise.race([
         patch('/messages/edit', null, payload.editMessage),
         timeout(SEND_TIMEOUT),
@@ -167,17 +182,24 @@ export const useSend = () => {
         chatDispatch({
           type: 'fail',
           payload: {
-            failTo: { type: 'EDIT', composeState: compose },
+            failTo: { type: 'EDIT' },
             key: payload.editMessage.messageId,
           },
         });
       }
     } else {
-      if (mediaId) payload.newMessage.mediaId = mediaId;
-      chatDispatch({
-        type: 'messageSending',
-        payload: { newMessage: payload.newMessage, sendTime: sendStartTime, media: null },
-      });
+      if (mediaId) {
+        payload.newMessage.mediaId = mediaId;
+        chatDispatch({
+          type: 'messageSending',
+          payload: {
+            newMessage: payload.newMessage,
+            sendTime: sendStartTime,
+            media: null,
+            composeState,
+          },
+        });
+      }
       const result = await Promise.race([
         post('/messages/send', null, payload.newMessage),
         timeout(SEND_TIMEOUT),
@@ -186,7 +208,7 @@ export const useSend = () => {
         chatDispatch({
           type: 'fail',
           payload: {
-            failTo: { type: 'SEND', composeState: compose },
+            failTo: { type: 'SEND' },
             key: payload.newMessage.previewId,
           },
         });
@@ -200,8 +222,8 @@ export const useSend = () => {
     checkComposeAtom,
     defaultDiceFaceRef,
     defaultInGame,
+    setBanner,
     intl,
-    channelMembersMap,
     channelId,
   ]);
 

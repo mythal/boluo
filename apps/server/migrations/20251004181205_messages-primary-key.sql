d85d11766acb0272e02ef267cb15024094d570c5
uonr <dev@yuru.me>
2025-10-06 15:42:42 +0900

    Revert "feat(server): change messages primary key to (channel_id, id)"
    This reverts commit b1fe8ee8fcbf7854cf1791cd0fab0efdc7c43938.

--- apps/server/migrations/20251004181205_messages-primary-key.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE IF EXISTS messages
-    DROP CONSTRAINT IF EXISTS messages_pkey;
-
-ALTER TABLE IF EXISTS messages
-    ADD CONSTRAINT messages_id_unique UNIQUE (id);
-
-ALTER TABLE IF EXISTS messages
-    ADD CONSTRAINT messages_pkey PRIMARY KEY (channel_id, id);

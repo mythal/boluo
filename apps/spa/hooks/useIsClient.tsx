a64356291056b0f05660e2f8b44292e19384c768
uonr <dev@yuru.me>
2025-10-11 05:24:51 +0900

    chore: disable lints

--- apps/spa/hooks/useIsClient.tsx
+++ apps/spa/hooks/useIsClient.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 export const useIsClient = () => {
   const [isClient, setIsClient] = useState(false);
+  // eslint-disable-next-line react-hooks/set-state-in-effect
   useEffect(() => setIsClient(true), []);
   return isClient;
 };

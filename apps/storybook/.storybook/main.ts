b3e4e38229f3409f1dfd8ca96b43b8a183549d9b
uonr <dev@yuru.me>
2025-10-15 22:13:49 +0900

    feat(storybook): favicon

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -10,6 +10,7 @@ function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
+  staticDirs: ['../public'],
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-docs'), getAbsolutePath('@storybook/addon-themes')],
   framework: { name: getAbsolutePath('@storybook/react-vite'), options: {} },

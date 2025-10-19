033aa08ff2fc7589980b279a36919f4a1a7ff9b4
uonr <dev@yuru.me>
2025-10-19 16:42:35 +0900

    feat(spa): login error messages

--- apps/spa/components/PaneLogin.tsx
+++ apps/spa/components/PaneLogin.tsx
@@ -1,9 +1,6 @@
-import { type ApiError } from '@boluo/api';
-import { useErrorExplain } from '@boluo/common/hooks/useErrorExplain';
 import { LogIn } from '@boluo/icons';
-import { type FC, useCallback } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
-import { useSetBanner } from '../hooks/useBanner';
+import { type FC } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { usePaneClose } from '../hooks/usePaneClose';
 import { LoginForm } from './account/LoginForm';
 import { PaneBox } from './PaneBox';
@@ -12,19 +9,6 @@ import { Todo } from './common/Todo';
 
 export const PaneLogin: FC = () => {
   const close = usePaneClose();
-  const setBanner = useSetBanner();
-  const intl = useIntl();
-  const errorExplain = useErrorExplain();
-  const handleError = useCallback(
-    (error: ApiError) => {
-      const content =
-        error.code === 'NO_PERMISSION'
-          ? intl.formatMessage({ defaultMessage: 'Username and password do not match' })
-          : errorExplain(error);
-      setBanner({ content, level: 'ERROR' });
-    },
-    [errorExplain, intl, setBanner],
-  );
   return (
     <PaneBox
       header={
@@ -34,7 +18,7 @@ export const PaneLogin: FC = () => {
       }
     >
       <div className="p-pane">
-        <LoginForm onSuccess={close} onError={handleError} className="w-full" />
+        <LoginForm onSuccess={close} className="w-full" />
         <div>
           <Todo>Sign Up</Todo>
         </div>

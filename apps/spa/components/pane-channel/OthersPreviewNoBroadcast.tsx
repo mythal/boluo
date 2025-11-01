bb4e4e527c39d07f14c94d4e26ad6f7190a14e35
uonr <dev@yuru.me>
2025-11-01 18:51:50 +0900

    feat(ui): <BroadcastTurnedOff />

--- apps/spa/components/pane-channel/OthersPreviewNoBroadcast.tsx
+++ apps/spa/components/pane-channel/OthersPreviewNoBroadcast.tsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { useRef, useState } from 'react';
+import { BroadcastTurnedOff } from '@boluo/ui/chat/BroadcastTurnedOff';
 
 interface Props {
   timestamp: number;
 }
 
 export const OthersPreviewNoBroadcast = React.memo<Props>(({ timestamp }: Props) => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    const now = new Date().getTime();
-    if (now - timestamp > 2000) return;
-    ref.current?.setAttribute('data-highlight', 'true');
-    const handle = window.setTimeout(() => {
-      ref.current?.setAttribute('data-highlight', 'false');
-    }, 200);
-    return () => window.clearTimeout(handle);
-  }, [timestamp]);
-  return (
-    <div
-      ref={ref}
-      className="text-text-secondary data-[highlight=true]:text-text-subtle transition-colors duration-100"
-    >
-      *
-      <span className="px-1 italic">
-        <FormattedMessage defaultMessage="Broadcast has been turned off" />
-      </span>
-      *
-    </div>
-  );
+  const prevTimestamp = useRef(timestamp);
+  const [count, setCount] = useState(0);
+  if (timestamp !== prevTimestamp.current) {
+    prevTimestamp.current = timestamp;
+    setCount((x) => x + 1);
+  }
+  return <BroadcastTurnedOff count={count} />;
 });
 
 OthersPreviewNoBroadcast.displayName = 'OthersPreviewNoBroadcast';

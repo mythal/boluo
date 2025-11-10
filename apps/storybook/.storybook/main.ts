4d341297b4706015ecfae5cdcb656c7d1cebbef3
uonr <dev@yuru.me>
2025-11-10 17:47:59 +0900

    chore: upgrade dependencies (#1007)
    * feat(storybook): storybook 19
    
    * feat: upgrade `@swc/plugin-formatjs`
    
    * chore: cargo update
    
    * feat: upgrade tokio-tungstenite
    
    * Revert "feat: upgrade `@swc/plugin-formatjs`"
    
    This reverts commit d5addb1fcd0efe44a1cb6b89d7f56de532f10449.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,6 +1,9 @@
+import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 import { join, dirname } from 'path';
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.

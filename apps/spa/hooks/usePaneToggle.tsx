da6cbee8ec95adf455d7132d9e70dbcf889cd748
uonr <dev@yuru.me>
2025-11-16 00:34:18 +0900

    feat(spa): toggle profile

--- apps/spa/hooks/usePaneToggle.tsx
+++ apps/spa/hooks/usePaneToggle.tsx
@@ -41,6 +41,8 @@ export const usePaneToggle = (props?: Props) => {
         let index: number = -1;
         if (pane.type === 'CHANNEL') {
           index = panes.findIndex((x) => x.type === 'CHANNEL' && x.channelId === pane.channelId);
+        } else if (pane.type === 'PROFILE') {
+          index = panes.findIndex((x) => x.type === 'PROFILE' && x.userId === pane.userId);
         } else if (pane.type === 'SPACE_MEMBERS' || pane.type === 'SPACE_SETTINGS') {
           index = panes.findIndex((x) => x.type === pane.type && x.spaceId === pane.spaceId);
         } else {

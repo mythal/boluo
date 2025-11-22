77c0cfd62a95d9cc9789b98727a8946662d5fb61
uonr <dev@yuru.me>
2025-11-23 00:49:08 +0900

    feat(spa): adjust `PaneDragHandle`

--- apps/spa/components/PaneDragHandle.tsx
+++ apps/spa/components/PaneDragHandle.tsx
@@ -1,25 +1,27 @@
-import { Activity, FC, useContext, useMemo } from 'react';
+import { Activity, FC, useContext } from 'react';
 import { usePaneDrag } from '../hooks/usePaneDrag';
 import { PaneContext } from '../state/view.context';
 import clsx from 'clsx';
 import { stopPropagation } from '@boluo/utils/browser';
-import GripVertical from '@boluo/icons/GripVertical';
 import { useIsChildPane } from '../hooks/useIsChildPane';
+import { useIntl } from 'react-intl';
 
 export const PaneDragHandle: FC = () => {
   const { focused: isFocused, key: paneKey } = useContext(PaneContext);
   const { canDrag, onHandlePointerDown, draggingPane } = usePaneDrag();
   const isChildPane = useIsChildPane();
-  const icon = useMemo(() => <GripVertical className="h-3 w-3" />, []);
+  const intl = useIntl();
   const isDraggingCurrentPane =
     draggingPane != null && draggingPane.key === paneKey && draggingPane.isChild === isChildPane;
+  const title = intl.formatMessage({ defaultMessage: 'Move Pane' });
   return (
     <Activity mode={!canDrag ? 'hidden' : 'visible'}>
       <button
         type="button"
-        aria-label="Reorder pane"
+        aria-label={title}
+        title={title}
         className={clsx(
-          'rounded: absolute left-1 inline-flex h-6 w-6 shrink-0 items-center justify-center',
+          'rounded: absolute left-0.5 inline-flex h-6 w-6 shrink-0 items-center justify-center',
           isDraggingCurrentPane ? 'bg-surface-strong opacity-70' : 'hover:bg-surface-strong',
           'cursor-grab active:cursor-grabbing',
           isFocused ? 'text-text-subtle' : 'text-text-subtle/50',
@@ -31,7 +33,7 @@ export const PaneDragHandle: FC = () => {
         }}
         onClick={stopPropagation}
       >
-        {icon}
+        <span className="not-sr-only">||</span>
       </button>
     </Activity>
   );

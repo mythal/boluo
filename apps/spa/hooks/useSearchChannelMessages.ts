5c0feaae9b1c374414395867fbb4f053532ef3b7
uonr <dev@yuru.me>
2025-12-01 03:28:47 +0900

    refactor(spa): typing

--- apps/spa/hooks/useSearchChannelMessages.ts
+++ apps/spa/hooks/useSearchChannelMessages.ts
@@ -1,7 +1,9 @@
 import type { ApiError, SearchDirection, SearchMessagesResult } from '@boluo/api';
 import { get } from '@boluo/api-browser';
-import useSWRInfinite, { type SWRInfiniteResponse } from 'swr/infinite';
+import useSWRInfinite, { SWRInfiniteFetcher, type SWRInfiniteResponse } from 'swr/infinite';
 import { unwrap } from '@boluo/utils/result';
+
+const path = '/messages/search' as const;
 
 export const useSearchChannelMessages = (
   channelId: string,
@@ -10,27 +12,36 @@ export const useSearchChannelMessages = (
 ): SWRInfiniteResponse<SearchMessagesResult, ApiError> => {
   const trimmedKeyword = keyword.trim();
 
-  return useSWRInfinite<SearchMessagesResult, ApiError>(
-    (pageIndex, previousPageData) => {
-      if (trimmedKeyword === '') {
-        return null;
-      }
-      if (pageIndex > 0 && previousPageData?.nextPos == null) {
-        return null;
-      }
-      const pos = pageIndex === 0 ? null : (previousPageData?.nextPos ?? null);
-      return ['/messages/search', channelId, trimmedKeyword, direction, pos] as const;
-    },
-    ([path, channel, searchKeyword, searchDirection, pos]) =>
-      get(path, {
-        channelId: channel,
-        keyword: searchKeyword,
-        direction: searchDirection,
-        pos,
-      }).then(unwrap),
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    },
-  );
+  const getKey = (
+    pageIndex: number,
+    previousPageData: SearchMessagesResult | null,
+  ): [typeof path, string, string, SearchDirection, number | null] | null => {
+    if (trimmedKeyword === '') {
+      return null;
+    }
+    if (pageIndex > 0 && previousPageData?.nextPos == null) {
+      return null;
+    }
+    const pos = pageIndex === 0 ? null : (previousPageData?.nextPos ?? null);
+    return [path, channelId, trimmedKeyword, direction, pos] as const;
+  };
+
+  const fetcher: SWRInfiniteFetcher<SearchMessagesResult, typeof getKey> = ([
+    path,
+    channel,
+    searchKeyword,
+    searchDirection,
+    pos,
+  ]) =>
+    get(path, {
+      channelId: channel,
+      keyword: searchKeyword,
+      direction: searchDirection,
+      pos,
+    }).then(unwrap);
+
+  return useSWRInfinite<SearchMessagesResult, ApiError>(getKey, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 };

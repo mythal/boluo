81abcf7080e4b0d6f8fb8632d8d1b8cd2291dca9
uonr <dev@yuru.me>
2025-12-02 01:04:07 +0900

    fix(spa): sidebar button

--- apps/spa/components/sidebar/SidebarButtonController.tsx
+++ apps/spa/components/sidebar/SidebarButtonController.tsx
@@ -1,26 +1,32 @@
 import { SidebarButton } from '@boluo/ui/chat/SidebarButton';
-import { FC } from 'react';
+import { type FC, useMemo } from 'react';
 import { isSidebarExpandedAtom, sidebarContentStateAtom } from '../../state/ui.atoms';
 import { useAtom, useAtomValue } from 'jotai';
-import { connectionStateAtom } from '../../state/chat.atoms';
+import { chatAtom, connectionStateAtom } from '../../state/chat.atoms';
+import { selectAtom } from 'jotai/utils';
 
 export const SidebarButtonController: FC = () => {
   const [isExpanded, setExpanded] = useAtom(isSidebarExpandedAtom);
+  const inSpace = useAtomValue(
+    useMemo(() => selectAtom(chatAtom, (chat) => chat.context.spaceId != ''), []),
+  );
   const connectionState = useAtomValue(connectionStateAtom);
   const [sidebarState, setSidebarState] = useAtom(sidebarContentStateAtom);
-  const disconnected = connectionState.type !== 'CONNECTED';
+  const disconnected = inSpace && connectionState.type !== 'CONNECTED';
+  const switchToConnections = useMemo(() => {
+    if (sidebarState === 'CONNECTIONS') {
+      return undefined;
+    }
+    return () => {
+      setSidebarState('CONNECTIONS');
+    };
+  }, [setSidebarState, sidebarState]);
   return (
     <SidebarButton
       isSidebarExpanded={isExpanded}
       setSidebarExpanded={setExpanded}
       disconnected={disconnected}
-      switchToConnections={
-        sidebarState === 'CONNECTIONS'
-          ? undefined
-          : () => {
-              setSidebarState('CONNECTIONS');
-            }
-      }
+      switchToConnections={switchToConnections}
     />
   );
 };

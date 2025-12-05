cacb217391cd3e426e3c235d7c1e65e17803c48d
uonr <dev@yuru.me>
2025-12-06 00:58:48 +0900

    feat(spa): action style italic

--- packages/ui/src/entities/EntityCodeBlock.tsx
+++ packages/ui/src/entities/EntityCodeBlock.tsx
@@ -14,7 +14,7 @@ export const EntityCodeBlock: FC<Props> = ({
 }) => {
   return (
     <div className="py-2">
-      <pre className="EntityCodeBlock border-border-default no-autospace font-pixel overflow-x-auto rounded border bg-black px-2 py-1 text-[15px] leading-snug text-green-300 shadow-sm">
+      <pre className="EntityCodeBlock border-border-default no-autospace font-pixel overflow-x-auto rounded border bg-black px-2 py-1 text-[15px] leading-snug text-green-300 not-italic shadow-sm">
         {source.substring(start, start + len)}
       </pre>
     </div>

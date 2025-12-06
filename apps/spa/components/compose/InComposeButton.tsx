781316e9a106ec6e146f1ec4dc2427075f5701b6
uonr <dev@yuru.me>
2025-12-06 16:27:36 +0900

    feat(tailwind-config): migrate dark theme from legacy

--- apps/spa/components/compose/InComposeButton.tsx
+++ apps/spa/components/compose/InComposeButton.tsx
@@ -19,7 +19,7 @@ export const InComposeButton = ({ onClick, children, label, pressed, disabled = 
         'InComposeButton',
         'text-action-secondary-text cursor-pointer rounded-sm bg-transparent p-[0.5em]',
         'hover:enabled:bg-action-primary-bg/20',
-        'pressed:bg-action-primary-bg/40',
+        'pressed:bg-action-secondary-bg-active',
         'disabled:bg-action-secondary-bg-disabled/50 disabled:text-action-secondary-text-disabled disabled:cursor-not-allowed',
       )}
       onClick={onClick}

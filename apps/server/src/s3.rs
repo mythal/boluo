8858e9f8717ac2001ac8631d88b7333d28de2eb7
uonr <dev@yuru.me>
2026-04-15 07:07:41 +0900

    refactor(server): use rusty-s3

--- apps/server/src/s3.rs
+++ apps/server/src/s3.rs
@@ -1,39 +1,32 @@
+use rusty_s3::{Bucket, Credentials, UrlStyle};
 use std::sync::OnceLock;
 
-use aws_sdk_s3::Config;
-
-fn make_config() -> Config {
-    use aws_credential_types::Credentials;
-    use std::env;
-    let access_key = env::var("S3_ACCESS_KEY_ID").expect("S3_ACCESS_KEY_ID is not set");
-    let secret_access_key =
-        env::var("S3_SECRET_ACCESS_KEY").expect("S3_SECRET_ACCESS_KEY is not set");
-    let endpoint_url = env::var("S3_ENDPOINT_URL").expect("S3_ENDPOINT_URL is not set");
-
-    let credentials = Credentials::new(access_key, secret_access_key, None, None, "boluo");
-    let credentials_provider =
-        aws_credential_types::provider::SharedCredentialsProvider::new(credentials);
-    Config::builder()
-        .behavior_version_latest()
-        // Fix DNS resolution error
-        .force_path_style(true)
-        .region(aws_sdk_s3::config::Region::new("auto"))
-        .endpoint_url(endpoint_url)
-        .credentials_provider(credentials_provider)
-        .build()
+pub fn get_bucket() -> &'static Bucket {
+    static BUCKET: OnceLock<Bucket> = OnceLock::new();
+    BUCKET.get_or_init(|| {
+        let endpoint = std::env::var("S3_ENDPOINT_URL").expect("S3_ENDPOINT_URL is not set");
+        let name = std::env::var("S3_BUCKET_NAME").expect("S3_BUCKET_NAME is not set");
+        Bucket::new(
+            endpoint.parse().expect("Invalid S3_ENDPOINT_URL"),
+            UrlStyle::Path,
+            name,
+            "auto",
+        )
+        .expect("Failed to initialize S3 bucket config")
+    })
 }
 
-fn get_config() -> &'static Config {
-    static SDK_CONFIG: OnceLock<Config> = OnceLock::new();
-    SDK_CONFIG.get_or_init(make_config)
+pub fn get_credentials() -> &'static Credentials {
+    static CREDENTIALS: OnceLock<Credentials> = OnceLock::new();
+    CREDENTIALS.get_or_init(|| {
+        let key = std::env::var("S3_ACCESS_KEY_ID").expect("S3_ACCESS_KEY_ID is not set");
+        let secret =
+            std::env::var("S3_SECRET_ACCESS_KEY").expect("S3_SECRET_ACCESS_KEY is not set");
+        Credentials::new(key, secret)
+    })
 }
 
-pub fn get_client() -> &'static aws_sdk_s3::Client {
-    static CLIENT: OnceLock<aws_sdk_s3::Client> = OnceLock::new();
-    CLIENT.get_or_init(|| aws_sdk_s3::Client::from_conf(make_config()))
-}
-
-pub fn get_bucket_name() -> &'static str {
-    static BUCKET_NAME: OnceLock<String> = OnceLock::new();
-    BUCKET_NAME.get_or_init(|| std::env::var("S3_BUCKET_NAME").expect("S3_BUCKET_NAME is not set"))
+pub fn get_http_client() -> &'static reqwest::Client {
+    static CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+    CLIENT.get_or_init(reqwest::Client::new)
 }
